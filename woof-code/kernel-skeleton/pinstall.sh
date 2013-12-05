a7152a868f9b8ecdcbdf15b97f21a0c0e98e7d49
mavrothal <mavrothal@yahoo.com>
2013-12-05 20:45:28 +0200

    Remove kernel 2.6.xx firmware

--- woof-code/kernel-skeleton/pinstall.sh
+++ woof-code/kernel-skeleton/pinstall.sh
@@ -23,21 +23,4 @@ fi
 #mv -f ./lib/modules/all-firmware/b43/lib/firmware/b43 ./lib/firmware/b43
 #120126 comment-out, fixed uevent replay for ssb at bootup, see http://bkhome.org/blog/?viewDetailed=02651
 
-#120310
-KERNMAJ3="`find ./lib/modules -mindepth 1 -maxdepth 1 -type d -name '3.*'`"
-if [ "$KERNMAJ3" ];then
- rm -rf ./lib/modules/all-firmware/ar9170usb #replaced by carl9170.
- sed -i -e '/^ar9170usb/d' ./etc/modules/firmware.dep
- rm -rf ./lib/modules/all-firmware/rt2860sta-fw #replaced by new firmware in rt2x00
- sed -i -e '/^rt2860sta/d' ./etc/modules/firmware.dep
- rm -rf ./lib/modules/all-firmware/rt2870sta-fw #replaced by new firmware in rt2x00
- sed -i -e '/^rt2870sta/d' ./etc/modules/firmware.dep
- rm -rf ./lib/modules/all-firmware/rtl8192se #firmware now in rtlwifi
- sed -i -e '/^rtl8192se/d' ./etc/modules/firmware.dep
-else
- rm -rf ./lib/modules/all-firmware/carl9170
- sed -i -e '/^carl9170/d' ./etc/modules/firmware.dep
- rm -rf ./lib/modules/all-firmware/rtlwifi #use older rtl8192se
- sed -i -e '/^rtlwifi/d' ./etc/modules/firmware.dep #use older rtl8192se
-fi
 
