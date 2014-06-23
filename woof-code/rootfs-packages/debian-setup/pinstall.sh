54f92e0c9b03e8e5afcfd4ff422f7b3ba7e4add0
jamesb <me@localhost>
2014-06-24 00:58:11 +0700

    fixes to build 32-bit debian sid and 64-bit ubuntu
    32-bit debian sid and 64-bit ubuntu trusty now works.
    debian-setup now creates initial en_US/en_AU locales (and their UTF-8 variants)
    README updated to confirm supported parent distros.

--- woof-code/rootfs-packages/debian-setup/pinstall.sh
+++ woof-code/rootfs-packages/debian-setup/pinstall.sh
@@ -5,3 +5,9 @@
 rm -f etc/init.d/udev                   # puppy already starts udev in rc.sysinit
 rm usr/bin/X; ln -sf Xorg usr/bin/X     # delete useless xorg wrapper
 ln -s xterm usr/bin/x-terminal-emulator # some programs look for this symlink
+
+echo Generating locale files
+chroot . /usr/bin/localedef --no-archive -i en_US en_US
+chroot . /usr/bin/localedef --no-archive -i en_AU en_AU
+chroot . /usr/bin/localedef --no-archive -i en_US -f UTF-8 en_US.UTF-8
+chroot . /usr/bin/localedef --no-archive -i en_AU -f UTF-8 en_AU.UTF-8
