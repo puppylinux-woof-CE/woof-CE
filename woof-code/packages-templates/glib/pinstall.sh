a33729363a773bcf13f1291722e82bc57ca26260
Dima Krasner <dima@dimakrasner.com>
2015-09-28 20:33:36 +0300

    Fixed the GLib template, for real.

--- woof-code/packages-templates/glib/pinstall.sh
+++ woof-code/packages-templates/glib/pinstall.sh
@@ -5,7 +5,7 @@ echo "Post-install script for glib..."
 for i in ./usr/lib/glib-2.0/*; do
 	[ ! -f "$i" ] && continue
 	name="`basename "$i"`"
-	[ ! -e "./usr/bin/$name" ] && ln -s "/usr/lib/glib-2.0/$name" /usr/bin/
+	[ ! -e "./usr/bin/$name" ] && ln -s "/usr/lib/glib-2.0/$name" ./usr/bin/
 done
 
 #also need this for slackware 13.1...
