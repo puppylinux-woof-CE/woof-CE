03d442431bda46331879994e4eec366be942716a
01micko <01micko@gmail.com>
2015-10-10 07:00:17 +1000

    Add a pinstall.sh to @zigbert 's ptheme package, deleting .desktop files for switch2 and/or GTK-chtheme

---
+++ woof-code/rootfs-packages/ptheme/pinstall.sh
@@ -0,0 +1,2 @@
+[ -f usr/share/applications/switch2.desktop ] && rm usr/share/applications/switch2.desktop
+[ -f usr/share/applications/GTK-chtheme.desktop ] && rm usr/share/applications/GTK-chtheme.desktop
