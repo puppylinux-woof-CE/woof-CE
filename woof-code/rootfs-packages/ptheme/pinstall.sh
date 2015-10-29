f65943048030c65a04abc1a80bf35b5d5c435dbb
zigbert <zigbert@sigmundberglund.com>
2015-10-29 19:37:16 +0100

    pTheme: remove icon_switcher.desktop file

--- woof-code/rootfs-packages/ptheme/pinstall.sh
+++ woof-code/rootfs-packages/ptheme/pinstall.sh
@@ -1,2 +1,3 @@
 [ -f usr/share/applications/switch2.desktop ] && rm usr/share/applications/switch2.desktop
 [ -f usr/share/applications/GTK-chtheme.desktop ] && rm usr/share/applications/GTK-chtheme.desktop
+[ -f usr/share/applications/icon_switcher.desktop ] && rm usr/share/applications/icon_switcher.desktop
