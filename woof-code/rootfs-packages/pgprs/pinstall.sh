357fd1c31e66e3b0b496609f5266ae489cf0b363
Richard Erwin <rerwin1@maine.rr.com>
2016-06-13 11:32:09 -0400

    Pgprs branch: Refine installation process (#805)
     Pgprs: Refine installation process

--- woof-code/rootfs-packages/pgprs/pinstall.sh
+++ woof-code/rootfs-packages/pgprs/pinstall.sh
@@ -1,40 +1,21 @@
 #!/bin/sh
 
-#Remove old pgprs components...
-if [ "$(pwd)" != "/" ] \
-  || [ -f /lib/modules/all-firmware/pgprs.tar.gz \
-       -o -d /lib/modules/all-firmware/pgprs ]; then
+#Create link to window icon, replacing placeholder.
+ln -snf puppy/wireless.svg usr/share/pixmaps/pgprs.svg
+
+[ "$(pwd)" = "/" ] || exit 0
+
+if [ -f /lib/modules/all-firmware/pgprs.tar.gz \
+  -o -d /lib/modules/all-firmware/pgprs ]; then
+ #Remove old pgprs components...
  rm -f etc/ppp/peers/gprs-connect-chatmm 2>/dev/null
  rm -f etc/ppp/peers/gprs-disconnect-chatmm 2>/dev/null
  rm -f etc/ppp/peers/gprsmm 2>/dev/null
  rm -f usr/share/applications/pgprs-setup.desktop 2>/dev/null
- if [ "$(pwd)" != "/" ];then
-  rm -fr lib/modules/all-firmware/pgprs 2>/dev/null
-  rm -f lib/modules/all-firmware/pgprs.tar.gz 2>/dev/null
- fi
-fi
-
-#Disable/remove old pgprs scripts in old location.
-if [ "$(pwd)" = "/" ] \
-  && [ ! -f /lib/modules/all-firmware/pgprs.tar.gz \
-  -a ! -d /lib/modules/all-firmware/pgprs ]; then
+else
+ #Disable/remove old pgprs scripts in old location.
  chmod -f a-x  usr/bin/pgprs-setup
  chmod -f a-x  usr/bin/pgprs-connect
-else
- rm -f usr/bin/pgprs-setup
- rm -f usr/bin/pgprs-connect
- [ "$(pwd)" != "/" ] && rm -f usr/sbin/pgprs-shell
-fi
-
-#Install gprs-editable only if not already present...
-if [ "$(pwd)" = "/" -a -f etc/ppp/peers/gprs-editable ];then
- rm -f etc/ppp/peers/gprs-editable.tmp
- [ "$(ls root/.packages/pgprs-*.files 2>/dev/null)" ] \
-  && sed -i '/gprs-editable\.tmp/d' root/.packages/pgprs-*.files
-else
- mv -f etc/ppp/peers/gprs-editable.tmp etc/ppp/peers/gprs-editable
- [ "$(ls root/.packages/pgprs-*.files 2>/dev/null)" ] \
-  && sed -i '/gprs-editable\.tmp/ s/\.tmp//' root/.packages/pgprs-*.files
 fi
 
 #Remove old state gprs.conf, to generate new one.
@@ -42,27 +23,22 @@ rm -f etc/gprs.conf
 rm -f root/.config/gprs.conf
 
 #Remove replaced options file, if not used by frisbee.
-[ -f usr/sbin/frisbee ] && rm -f etc/ppp/options.gprs
+[ -f usr/local/bin/frisbee ] && rm -f etc/ppp/options.gprs
 
 #Change connectwizard pgprs default to connectwizard.
-if [ "$(pwd)" = "/" ];then
- grep 'pgprs' usr/local/bin/defaultconnect \
-  && echo -e "#!/bin/sh\nexec connectwizard" > usr/local/bin/defaultconnect
-fi
+grep 'pgprs' usr/local/bin/defaultconnect \
+ && echo -e "#!/bin/sh\nexec connectwizard" > usr/local/bin/defaultconnect
 
 #For very old puppies, revert the menu category.
 ! grep -qs 'X-Network-phone' etc/xdg/menus/puppy-network.menu \
  && grep -qs '>Dialup<' etc/xdg/menus/puppy-network.menu \
  && sed -i 's/X-Network-phone/Dialup/' usr/share/applications/pgprs-connect.desktop
 
-#Create link to appropriate window icon.
+#Change link to appropriate window icon.
 rm -f usr/local/lib/X11/mini-icons/pgprs.png #precaution
-rm -f usr/share/pixmaps/pgprs.svg #placeholder
-if [ -f usr/share/pixmaps/puppy/wireless.svg ];then
- ln -snf puppy/wireless.svg usr/share/pixmaps/pgprs.svg
-else
+if [ ! -f usr/share/pixmaps/puppy/wireless.svg ];then
+ rm -f usr/share/pixmaps/pgprs.svg
  ln -snf /usr/local/lib/X11/mini-icons/Pwireless.png usr/share/pixmaps/pgprs.png
- [ "$(ls root/.packages/pgprs-*.files 2>/dev/null)" ] \
-  && sed -i '/pgprs\.svg/ s/svg/png/' root/.packages/pgprs-*.files
+ sed -i '/pgprs\.svg/ s/svg/png/' root/.packages/pgprs-*.files
  sed -i '/pgprs\.svg/ s/svg/png/' usr/share/applications/pgprs-connect.desktop
 fi
