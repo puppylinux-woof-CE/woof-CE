59a5188540eed836ce27bfff0cfd7fc2c307d78e
wdlkmpx <wdlkmpx@gmail.com>
2016-07-17 23:53:22 +0000

    initrd-progs: add util-linux 2.22

--- initrd-progs/build.sh
+++ initrd-progs/build.sh
@@ -292,6 +292,7 @@ function check_bin() {
 		fuse_static) static_bins='fusermount' ;;
 		module-init-tools_static) static_bins='lsmod modprobe' ;;
 		util-linux_static) static_bins='losetup' ;;
+		util-linux-222_static) static_bins='losetup-222' ;;
 		*) static_bins=${init_pkg%_*} ;;
 	esac
 	for sbin in ${static_bins} ; do
