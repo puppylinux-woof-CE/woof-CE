f380d486bce0da87a6d1b35774c53a0470e50914
wdlkmpx <wdlkmpx@gmail.com>
2016-09-22 16:20:37 +0000

    fixes for the bundled fake 'gettext.sh'
    also add /usr/local/bin to $PATH in  rc.shutdown

--- woof-code/rootfs-skeleton/usr/local/bin/gettext.sh
+++ woof-code/rootfs-skeleton/usr/local/bin/gettext.sh
@@ -4,18 +4,46 @@
 # - some people might not want to use gettext at all
 # - scripts may be faster by using this
 
-eval_gettext () {
-	eval echo -n "$1"
+# GUI scripts have "\Z" and \ is lost after eval
+#   so there's a workaround for that
+
+# Scripts use arguments with more than one line
+#   eval tries to execute the 2nd line and so on
+#   to fix this, pass a variable without ""
+#   as that method passes spaces instead of lines
+
+eval_gettext() {
+	msg="$1"
+	[ "$msg" ] || return 1
+	#Convert "\Z" to "~Z"
+	msg="${msg//\\\Z/~Z}"
+	# Eval
+	msg="$(eval echo -n $msg)"
+	#Convert "~Z" to "\Z"
+	msg="${msg//~Z/\\\Z}"
+	#deliver msg
+	echo "$msg"
 }
 
 # needs 3-4 arguments
 eval_ngettext () {
+	msg=
 	[ "$5" ] && return 1
 	if [ "$4" ] ; then
-		eval echo -n "$3"
+		msg="$3"
 	elif [ "$3" ] ; then
-		eval echo -n "$2"
+		msg="$2"
 	fi
+	[ "$msg" ] || return 1
+	msg="$1"
+	#Convert "\Z" to "~Z"
+	msg="${msg//\\\Z/~Z}"
+	# Eval
+	msg="$(eval echo -n $msg)"
+	#Convert "~Z" to "\Z"
+	msg="${msg//~Z/\\\Z}"
+	#deliver msg
+	echo "$msg"
 }
 
 ### END ###
