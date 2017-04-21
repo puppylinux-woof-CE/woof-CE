aaf678dd21b709a33df2871279a6cd6528618cd9
01micko <01micko@gmail.com>
2017-04-21 11:38:02 +1000

    fix minor error

--- woof-code/packages-templates/xorg_base_new/pinstall.sh
+++ woof-code/packages-templates/xorg_base_new/pinstall.sh
@@ -5,7 +5,7 @@ echo "xorg_base post-install script..."
 fndLIBGL="`find ./usr/lib ./usr/X11/lib -maxdepth 1 -name libGL*`"
 
 if [ "$fndLIBGL" ];then
- sed -i -e 's%.*#LOADGLX%#    Disable    "glx" #LOADGLX%' etc/X11/xorg.conf0
+ [ -f etc/X11/xorg.conf0 ] && sed -i -e 's%.*#LOADGLX%#    Disable    "glx" #LOADGLX%' etc/X11/xorg.conf0
 fi
 
 echo "Removing legacy X11R7 directory and links"
