57ca192160d3b60ce21c5844c70d15741acedaf8
wdlkmpx <wdlkmpx@gmail.com>
2017-07-20 19:11:24 +0800

    simplify lib* pkg templates

--- woof-code/packages-templates/libpciaccess/pinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#get rid of this as in xorg 7.5 it no longer works properly...
-#(use lspci instead)
-if [ -f ./usr/share/pci.ids -o -f ./usr/share/pci.ids.gz ];then
- rm -f ./usr/bin/scanpci
-fi
