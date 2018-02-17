8b3e48f678db04afe0c524764013fb0eea34fd6d
wdlkmpx <wdlkmpx@gmail.com>
2018-02-17 14:03:07 +0800

    delete kompozer pkg template
    pkg not provided by any supported compat distro..

--- woof-code/packages-templates/kompozer/pinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! "`pwd`" = "/" ];then
-
-  echo '#!/bin/sh' > ./usr/local/bin/defaulthtmleditor
-  echo 'exec kompozer "$@"' >> ./usr/local/bin/defaulthtmleditor
-  chmod 755 ./usr/local/bin/defaulthtmleditor
-
-fi
