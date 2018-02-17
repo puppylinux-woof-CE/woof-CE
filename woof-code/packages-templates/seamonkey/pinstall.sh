bca1ba22e07b338edf24061e7583799c127105d0
wdlkmpx <wdlkmpx@gmail.com>
2018-02-17 15:01:54 +0800

    remove duplicate seamonkey pkg template

--- woof-code/packages-templates/seamonkey/pinstall.sh
+++ woof-code/packages-templates/seamonkey/pinstall.sh
@@ -1,29 +1,12 @@
 #!/bin/sh
-#post-install script for Mozilla.
-#assume current directory is rootfs-complete, which has the final filesystem.
-
-if [ ! "`pwd`" = "/" ];then
-
- echo "Configuring Mozilla SeaMonkey browser..."
-
-  echo '#!/bin/sh' > ./usr/local/bin/defaultbrowser
-  echo 'exec mozstart "$@"' >> ./usr/local/bin/defaultbrowser
-
-  if [ ! -f usr/local/bin/gtkmoz ];then
-   ln -s mozstart usr/local/bin/gtkmoz
-  fi
-
-  echo '#!/bin/sh' > ./usr/local/bin/defaultemail
-  echo 'exec mozmail "$@"' >> ./usr/local/bin/defaultemail
-
-  echo '#!/bin/sh' > ./usr/local/bin/defaulthtmleditor
-  echo 'exec mozedit "$@"' >> ./usr/local/bin/defaulthtmleditor
-
-  echo '#!/bin/sh' > ./usr/local/bin/defaultcontact
-  echo 'exec mozaddressbook "$@"' >> ./usr/local/bin/defaultcontact
-
- echo "Note that mozilla and netscape all point to mozstart in /usr/local/bin"
- echo "...ok, setup for Mozilla (Seamonkey)."
- echo -n "seamonkey" > /tmp/rightbrwsr.txt
-
-fi
+echo '#!/bin/sh
+exec seamonkey "$@"' > usr/local/bin/defaultbrowser
+chmod 755 usr/local/bin/defaultbrowser
+echo '#!/bin/sh
+exec seamonkey "$@"' > usr/local/bin/defaulthtmlviewer
+chmod 755 usr/local/bin/defaulthtmlviewer
+echo "setting up SeaMonkey browser"
+echo '#!/bin/sh
+exec seamonkey -mail "$@"' > usr/local/bin/defaultemail
+chmod 755 usr/local/bin/defaultemail
+echo "setting up SeaMonkey email"
