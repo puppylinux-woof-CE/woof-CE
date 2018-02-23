49492ec7dd83d248d7febb85872ed9a8498f4af0
wdlkmpx <wdlkmpx@gmail.com>
2018-02-23 19:07:50 +0800

    simplify more pkg templates..

--- woof-code/packages-templates/geany/pinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#post-install script.
-
-if [ ! "`pwd`" = "/" ];then
-
- echo '#!/bin/sh' > ./usr/local/bin/defaulttexteditor
- echo 'exec geany "$@"' >> ./usr/local/bin/defaulttexteditor
-
-fi
