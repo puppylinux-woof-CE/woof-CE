d6ead74553dea84f04a3690dec79ff593d8475a5
wdlkmpx <wdlkmpx@gmail.com>
2018-02-25 17:02:49 +0800

    new boot param: intel_uxa, inteluxa or uxa
    so that @gyrog can boot to a usable desktop...

--- woof-code/rootfs-skeleton/usr/sbin/fix_intel_hd.sh
+++ woof-code/rootfs-skeleton/usr/sbin/fix_intel_hd.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
-
-# this may fix intel crashes on some chips
-# see http://www.murga-linux.com/puppy/viewtopic.php?p=955636#955636
-
-echo "fixing intel graphics"
-cat > /etc/X11/xorg.conf.d/20-intel.conf <<EOF
-Section "Device" 
-  Identifier "Card0" 
-  Driver "intel" 
-  Option "AccelMethod" "uxa" 
-EndSection 
-EOF
-
-echo "please restart X"
+xorg_intel_uxa.sh
