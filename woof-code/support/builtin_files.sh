3b1b90d260a60ec10a7a9e5702aad8c01927bd72
wdlkmpx <wdlkmpx@gmail.com>
2018-04-18 20:20:47 +0800

    Revert "3buildditro-Z: build a .pet with lists of all devx builtin files..."
    This reverts commit 151db0b75e51323f52f63d7fa88c48c5cf67390f.
    
    doubts.. this can be reverted anytime though

--- woof-code/support/builtin_files.sh
+++ woof-code/support/builtin_files.sh
@@ -1,105 +1,90 @@
-#!/bin/bash
-# * called by 3builddistro
-# * we're in ./sandbox3 or .
-# * processing happens in ./sandbox3
+#!/bin/sh
+# * sourced by 3builddistro
+# * we're in sandbox3
 
-#===== need to get build info =====
-if [ ! -f _00build.conf ] ; then
+if [ "$BUILDSYS" != "yes" ] ; then
+	echo "* standalone *"
 	cd ..
-fi
-. ./_00build.conf
-. ./_00func
-. ./DISTRO_SPECS
-source_compat_repos
-source_pkgs_specs
-set_binaries_var
-set_archdir_var
-cd sandbox3 || exit 1
-#==================================
-
-TDIR="rootfs-complete"
-
-if [ "$1" = "dev" -a -d devx ] ; then
-	DEVXP='_DEV'
-	TDIR='devx'
+	. ./_00build.conf
+	. ./_00func
+	. ./DISTRO_SPECS
+	source_compat_repos
+	source_pkgs_specs
+	set_binaries_var
+	set_archdir_var
+	cd sandbox3
 fi
 
 #======================================================================================
 
 #100527 build a .pet with lists of all builtin files...
 echo
-echo "Now building sandbox3/0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}.pet,"
+echo "Now building sandbox3/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}.pet,"
 echo "which is a PET package with lists of all packages and files builtin to the SFS..."
-rm   -rf 0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION} 2>/dev/null
-mkdir -p 0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}/root/.packages/builtin_files
-mkdir -p /tmp/0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}
-
+rm -rf 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION} 2>/dev/null
+mkdir 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}
+mkdir -p /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}
 for ONEGENDIR in $(find -H ../packages-${DISTRO_FILE_PREFIX} -maxdepth 1 -type d | sort)
 do
 	ONEGENNAME=${ONEGENDIR##*/} #basename $ONEGENDIR
 	if ! ( echo "$PKGS_SPECS_TABLE" | grep -q "^yes|${ONEGENNAME}|" ) ; then
 		continue
 	fi
-	if [ "${DEVXP}" ] ; then
-		if [ -d ../packages-${DISTRO_FILE_PREFIX}/${ONEGENNAME}_DEV ] ; then
-			ONEGENNAME=${ONEGENNAME}_DEV
-			ONEGENDIR=${ONEGENDIR}_DEV
-		else
-			continue
-		fi
-	fi
 	echo -n "$ONEGENNAME "
-	#          ignore top-level files
-	find -H $ONEGENDIR -mindepth 2 \( -type f -o -type l \) 2>/dev/null | \
-		sed -e "s%^../packages-${DISTRO_FILE_PREFIX}/${ONEGENNAME}/%/%" | \
-			sort > /tmp/0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}/${ONEGENNAME}.files
+	find -H $ONEGENDIR -type f -o -type l | \
+		sed -e "s%^\\.\\./packages-${DISTRO_FILE_PREFIX}/${ONEGENNAME}/%/%" | \
+		sort > /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/${ONEGENNAME}.files
 	sync
+	mkdir -p 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/root/.packages/builtin_files
 	(
-	while read ONELINE ; do
-		if [ -e "${TDIR}${ONELINE}" ];then
-			[ -d "${TDIR}${ONELINE}" ] && continue #ignore symlinks to directories
-			echo "${ONELINE}" #only files that are in ${TDIR}
+	while read ONELINE
+	do
+		NEWPATH=${ONELINE%/*} #dirname "$ONELINE"
+		[ "${NEWPATH}" = "" ] && continue #ignore top-level files.
+		if [ -e "rootfs-complete${ONELINE}" ];then
+			[ -d "rootfs-complete${ONELINE}" ] && continue #a symlink to a directory
+			echo "${ONELINE}" #only files that are in rootfs-complete
 		fi
-	done < /tmp/0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}/${ONEGENNAME}.files
-	) > 0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}/root/.packages/builtin_files/${ONEGENNAME}
+	done < /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/${ONEGENNAME}.files
+	) > 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/root/.packages/builtin_files/${ONEGENNAME}
 done
 
-# do the same for rootfs-packages.. does not apply to devx.
-if [ ! "${DEVXP}" -a -f /tmp/rootfs-packages.specs ] ; then
+# do the same for rootfs-packages
+if [ -f /tmp/rootfs-packages.specs ];then
 	while read line
 	do
 		PKGL=`echo $line | cut -d '|'  -f 2`
 		echo -n "${PKGL} "
-		find -H ../rootfs-packages/$PKGL -mindepth 2 \( -type f -o -type l \) 2>/dev/null | \
-			sed -e "s%^../rootfs-packages/${PKGL}/%/%" | \
+		find -H ../rootfs-packages/$PKGL -type f -o -type l | \
+			sed -e "s%^\\.\\./rootfs-packages/${PKGL}/%/%" | \
 			sort > /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/${PKGL}.files
 		sync
 		mkdir -p 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/root/.packages/builtin_files
-		while read ONELINE ; do
-			if [ -e "${TDIR}${ONELINE}" ];then
-				[ -d "${TDIR}${ONELINE}" ] && continue #a symlink to a directory
+		while read ONELINE
+		do
+			case "$ONELINE" in .*) continue ;; esac #catch ex: ../packages-qrky/abiword
+			NEWPATH=${ONELINE%/*} #dirname "$ONELINE"
+			[ "${NEWPATH}" = "" ] && continue #ignore top-level files.
+			if [ -e "rootfs-complete${ONELINE}" ];then
+				[ -d "rootfs-complete${ONELINE}" ] && continue #a symlink to a directory
 				echo "${ONELINE}" >> 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/root/.packages/builtin_files/${PKGL}
 			fi
 		done < /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/${PKGL}.files
 	done < /tmp/rootfs-packages.specs
 	rm -f /tmp/rootfs-packages.specs
 fi
-
 echo
-echo "0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}|0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}|${DISTRO_VERSION}||BuildingBlock|||0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}.pet||Lists of files built-in to the SFS file||||
-" > 0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}/pet.specs
-rm -f 0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}.pet 2>/dev/null
+echo "0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}|0builtin_files_${DISTRO_FILE_PREFIX}|${DISTRO_VERSION}||BuildingBlock|||0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}.pet||Lists of files built-in to the SFS file||||
+" > 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/pet.specs
+rm -f 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}.pet 2>/dev/null
 
-dir2tgz 0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}
-tgz2pet 0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}.tar.gz
-rm -rf /tmp/0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}
+dir2tgz 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}
+tgz2pet 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}.tar.gz
+rm -rf /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}
 echo
-
-mkdir -p ${TDIR}/root/.packages
-
-echo "installing pkg lists into ${TDIR}/root/.packages/builtin_files..."
-cp -a -f 0builtin_files_${DISTRO_FILE_PREFIX}${DEVXP}-${DISTRO_VERSION}/root/.packages/builtin_files \
-			${TDIR}/root/.packages
+echo "installing pkg lists into rootfs-complete/root/.packages/builtin_files..."
+cp -a -f 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/root/.packages/builtin_files \
+	rootfs-complete/root/.packages
 echo '...done'
 
 ### END ###
