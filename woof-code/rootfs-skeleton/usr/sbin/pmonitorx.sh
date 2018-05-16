e8c38d7096d2999733869345230cb3334b9cd86d
wdlkmpx <wdlkmpx@gmail.com>
2018-05-16 09:25:12 +0800

    fix 'pmonitork.sh' and 'pmonitorx.sh'
    these 2 scripts should be deleted..

--- woof-code/rootfs-skeleton/usr/sbin/pmonitorx.sh
+++ woof-code/rootfs-skeleton/usr/sbin/pmonitorx.sh
@@ -3,14 +3,4 @@
 
 #Xdialog --title "Monitoring tail of $1" --smooth --fixed-font --no-cancel --ok-label "Exit" --tailbox $1 18 95
 
-LINES1=0
-LINES2=0
-while [ 1 ];do
- LINES2=`wc -l /tmp/xerrs.log | tr -s " " | cut -f 2 -d " "`
- if [ $LINES2 -gt $LINES1 ];then
-  LINESDIFF=`expr $LINES2 - $LINES1`
-  tail -n $LINESDIFF /tmp/xerrs.log
-  LINES1=$LINES2
- fi 
- sleep 1
-done
+tail -f /tmp/xerrs.log
