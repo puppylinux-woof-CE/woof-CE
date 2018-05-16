ec4ddb0aa8e1bb1da371da658a199b8b96b90f11
wdlkmpx <wdlkmpx@gmail.com>
2018-05-16 09:25:15 +0800

    updatenetmoduleslist.sh: remove old code / misc edits..

--- woof-code/rootfs-skeleton/usr/sbin/updatenetmoduleslist.sh
+++ woof-code/rootfs-skeleton/usr/sbin/updatenetmoduleslist.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 #Barry Kauler 2009
 #w001 now in /usr/sbin in the distro, called from /etc/rc.d/rc.update.
-#w474 bugfix for 2.6.29 kernel, modules.dep different format.
-#w478 old k2.6.18.1 has madwifi modules (ath_pci.ko) in /lib/modules/2.6.18.1/net.
-#v423 now using busybox depmod, which generates modules.dep in "old" format.
 #111027 make modinfo quiet.
-#120507 improve kernel version test. add 'sdio' interfaces.
 
 KERNVER="`uname -r`"
 DRIVERSDIR="/lib/modules/$KERNVER/kernel/drivers/net"
 
 echo "Updating /etc/networkmodules..."
 
-DEPFORMAT='new'
-if vercmp $KERNVER lt 2.6.29; then #120507
-	DEPFORMAT='old'
-fi
+( #> /tmp/nm_rawlist$$
 
-#v423 need better test, as now using busybox depmod...
-[ "`grep '^/lib/modules' /lib/modules/${KERNVER}/modules.dep`" != "" ] && DEPFORMAT='old'
+grep "^kernel/drivers/net/" /lib/modules/${KERNVER}/modules.dep | \
+	sed -e 's/\.gz:/:/' | cut -f 1 -d ':'
 
-if [ "$DEPFORMAT" = "old" ];then
-	OFFICIALLIST="`cat /lib/modules/${KERNVER}/modules.dep | grep "^/lib/modules/$KERNVER/kernel/drivers/net/" | sed -e 's/\.gz:/:/' | cut -f 1 -d ':'`"
-else
-	OFFICIALLIST="`cat /lib/modules/${KERNVER}/modules.dep | grep "^kernel/drivers/net/" | sed -e 's/\.gz:/:/' | cut -f 1 -d ':'`"
-fi
-
-#there are a few extra scattered around... needs to be manually updated...
-EXTRALIST="extra/acx.ko
+# there are a few extra scattered around... needs to be manually updated...
+echo "acx.ko
 extra/rt2400.ko
 extra/rt2500.ko
 extra/rt2570.ko
@@ -44,49 +31,38 @@ linux-wlan-ng/prism2_pci.ko
 linux-wlan-ng/prism2_plx.ko
 r8180/r8180.ko
 "
-RAWLIST="$OFFICIALLIST
-$EXTRALIST"
+
+) | sed 's%.*/%% ; s%\.ko.*%%' | sort -u > /tmp/nm_rawlist$$
 
 #the list has to be cutdown to genuine network interfaces only...
-echo -n "" > /tmp/networkmodules
+
 (
- echo "$RAWLIST" | while read ONERAW
+ while read ONEBASE
  do
-
-	[ "$ONERAW" = "" ] && continue #precaution
-
-	#ONEBASE="`basename $ONERAW .ko`"
-	#ONEINFO="`modinfo $ONEBASE 2>/dev/null | tr '\t' ' ' | tr -s ' '`" #111027 make it quiet.
-	#ONETYPE="`echo "$ONEINFO" | grep '^alias:' | head -n 1 | cut -f 2 -d ' ' | cut -f 1 -d ':'`"
-	#ONEDESCR="`echo "$ONEINFO" | grep '^description:' | head -n 1 | cut -f 2 -d ':'`"
-
-	ONEBASE=${ONERAW##*/}
-	ONEBASE=${ONEBASE%.ko}
+	[ -z "$ONEBASE" ] && continue #precaution
 	ONETYPE=""
 	ONEDESCR=""
-	MODINFO=$(modinfo $ONEBASE 2>/dev/null)
-	[ "$MODINFO" = "" ] && continue
-
-	while read line ; do
-		case $line in
-			"description:"*) read -r zz ONEDESCR <<< "$line" ;;
-			"alias:"*) read -r alias ONETYPE <<< "$line" ; break ;;
+	MODINFO=$(modinfo $ONEBASE 2>/dev/null) || continue
+	# -z "$MODINFO" ] && continue
+	#-
+	while read F1 F2plus ; do
+		case $F1 in
+			"description:") ONEDESCR=$F2plus ;;
+			"alias:") ONETYPE=${F2plus%%:*} ; break ;;
 		esac
 	done <<< "$MODINFO"
-
-	ONETYPE=${ONETYPE%%:*}
+	#-
 	case "$ONETYPE" in pci|pcmcia|usb|ssb|sdio)
 		#ssb=b43legacy.ko...  sdio=sdio interfaces...
-		echo "Adding $ONEBASE" >&2
+		#echo "Adding $ONEBASE" >&2
 		echo -e "$ONEBASE \"$ONETYPE:  $ONEDESCR\""
 	esac
-
- done
-) | sort -u > /etc/networkmodules-${KERNVER}
+ done < /tmp/nm_rawlist$$
+) > /etc/networkmodules-${KERNVER}
 
 # the generated networkmodules includes the kernel version
 # /etc/networkmodules is a symlink to that file
-rm -f /etc/networkmodules
+rm -f /etc/networkmodules /tmp/nm_rawlist$$
 ln -sv networkmodules-${KERNVER} /etc/networkmodules
 
 ### END ###
