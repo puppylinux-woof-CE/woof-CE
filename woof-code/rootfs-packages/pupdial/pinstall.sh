bd0ef94717d51e00bdf613ea948763de605a5139
rserwin1 <richard_erwin@msn.com>
2018-08-04 18:42:06 -0400

    pupdial: Update moved components to become version 2.0 (AFTER skeleton deletions)

---
+++ woof-code/rootfs-packages/pupdial/pinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ "$(pwd)" = "/" ];then
+ #Remove copies from old location.
+ rm -f usr/sbin/gen_modem_init_string
+ rm -f usr/sbin/modemdisconnect
+ rm -f usr/sbin/modemprobe
+ rm -f usr/sbin/modemprobe_erase
+ rm -f usr/sbin/modemprobe_help
+ rm -f usr/sbin/modemtest
+ rm -f usr/sbin/pupdial
+ rm -f usr/sbin/pupdial_init_hotpluggable
+ rm -f usr/sbin/pupdial_wizard_helper
+fi
+
+#Replace placeholder with link.
+ln -snf ../local/pupdial/pupdial /usr/sbin/
