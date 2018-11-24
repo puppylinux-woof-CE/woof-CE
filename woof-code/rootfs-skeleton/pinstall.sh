6055c9019c31047161ed40e7181c6783dc2ee2c9
wdlkmpx <wdlkmpx@gmail.com>
2018-11-24 11:54:21 +0800

    woof-code/rootfs-skeleton/pinstall.sh: cleanup

--- woof-code/rootfs-skeleton/pinstall.sh
+++ woof-code/rootfs-skeleton/pinstall.sh
@@ -2,47 +2,26 @@
 #post-install script.
 #Puppy Linux
 #assume current directory is rootfs-complete, which has the final filesystem.
-#this script is similar to the post-install scripts of the window managers.
-#Keywords are located in the Help page and the lines uncommented.
-#DISTRO_VERSION, DISTRO_NAME are global variables visible here.
-#110422 DISTRO_VERSION variable now has dotted format. note, also now using full dotted version# in puppy filenames.
-#120225 create symlink release-notes.htm to actual release-notes file. NO.
-#120818 now have /etc/xdg in Woof, taken out of xdg_puppy PET, relocated pinstall.sh to here.
-#132211 removed icewm template from default, who uses that anyway?
 
 . etc/DISTRO_SPECS
 
-WKGDIR="`pwd`"
-
 echo "Configuring Puppy skeleton..."
-
-#cleanup...
-rm -f /tmp/fbvideomode.txt
-
 echo "Configuring Puppy Help page..."
-
-cp -f usr/share/doc/index.html.top usr/share/doc/index.html.top-raw #120225
-cp -f usr/share/doc/index.html.bottom usr/share/doc/index.html.bottom-raw #120225
-cp -f usr/share/doc/home.htm usr/share/doc/home-raw.htm #120225
 
 cutDISTRONAME="`echo -n "$DISTRO_NAME" | cut -f 1 -d ' '`"
 cPATTERN="s/cutDISTRONAME/${cutDISTRONAME}/g"
-RIGHTVER="$DISTRO_VERSION"
 PUPPYDATE="`date | tr -s " " | cut -f 2,6 -d " "`"
 RELEASE_DATE="`date "+%B, %Y"`"
 dPATTERN="s/PUPPYDATE/${PUPPYDATE}/g"
 rPATTERN="s/RELEASE_DATE/${RELEASE_DATE}/g"
-
-echo "Writing version number and distro name and date to Help page..."
-PATTERN1="s/RIGHTVER/${RIGHTVER}/g"
 PATTERN2="s/DISTRO_VERSION/${DISTRO_VERSION}/g"
 nPATTERN="s/DISTRO_NAME/${DISTRO_NAME}/g"
-sed -i -e "$PATTERN1" -e "$PATTERN2" -e "$nPATTERN" -e "$dPATTERN" -e "$cPATTERN" usr/share/doc/index.html.top
-sed -i -e "$PATTERN1" -e "$PATTERN2" -e "$nPATTERN" -e "$dPATTERN" usr/share/doc/index.html.bottom
+sed -i -e "$PATTERN2" -e "$nPATTERN" -e "$dPATTERN" -e "$cPATTERN" usr/share/doc/index.html.top
+sed -i -e "$PATTERN2" -e "$nPATTERN" -e "$dPATTERN" usr/share/doc/index.html.bottom
 
 (
-cat usr/share/doc/index.html.top
-cat usr/share/doc/index.html.bottom
+	cat usr/share/doc/index.html.top
+	cat usr/share/doc/index.html.bottom
 ) > usr/share/doc/index.html
 
 echo "Writing distro name to jumping-off page..."
@@ -51,10 +30,7 @@ sed -i -e "$nPATTERN" usr/share/doc/home.htm
 echo "Creating base release notes..."
 if [ ! -e usr/share/doc/release-${cutDISTRONAME}-${DISTRO_VERSION}.htm ];then
 	mv -f usr/share/doc/release-skeleton.htm usr/share/doc/release-${cutDISTRONAME}-${DISTRO_VERSION}.htm
-	sed -i -e "$PATTERN1" -e "$PATTERN2" -e "$nPATTERN" -e "$rPATTERN" usr/share/doc/release-${cutDISTRONAME}-${DISTRO_VERSION}.htm
-fi
-if [ ! -e usr/share/doc/release-${cutDISTRONAME}-${RIGHTVER}.htm ];then
-	ln -s release-${cutDISTRONAME}-${DISTRO_VERSION}.htm usr/share/doc/release-${cutDISTRONAME}-${RIGHTVER}.htm
+	sed -i -e "$PATTERN2" -e "$nPATTERN" -e "$rPATTERN" usr/share/doc/release-${cutDISTRONAME}-${DISTRO_VERSION}.htm
 fi
 
 # write extra stuff for release notes
@@ -71,63 +47,24 @@ if [ -f ../../support/release_extras/"${DISTRO_FILE_PREFIX}.htm" ];then
 		ctrl=$(($ctrl + 1))
 	done < usr/share/doc/release-${cutDISTRONAME}-${DISTRO_VERSION}.htm
 	cat "../../support/release_extras/${DISTRO_FILE_PREFIX}.htm" >> /tmp/release.htm
-	sed -i "s/DISTRO_VERSION/$DISTRO_VERSION/g" /tmp/release.htm
+	sed -i -e "s/DISTRO_VERSION/$DISTRO_VERSION/g" -e "$rPATTERN" /tmp/release.htm
 	cat /tmp/release.bottom >> /tmp/release.htm
 	cp -af /tmp/release.htm usr/share/doc/release-${cutDISTRONAME}-${DISTRO_VERSION}.htm
-fi
-
-#ln -snf release-${cutDISTRONAME}-${DISTRO_VERSION}.htm usr/share/doc/release-notes.htm #120225
-
-#echo "The default kernel for Puppy is vmlinuz."
-echo -n "vmlinuz" > /tmp/vmlinuzforpup.txt
-#note, createpuppy will read this.
-
-#120818 now have /etc/xdg in Woof, taken out of xdg_puppy PET, relocated pinstall.sh to here...
-#this code is to fix the icewm menu...
-if [ -f ./etc/xdg/templates/_root_.icewm_menu ];then
- if [ "`find ./usr/local/bin ./usr/bin ./usr/sbin ./usr/X11R7/bin -maxdepth 1 -type f -name evilwm`" = "" ];then
-  grep -v ' evilwm$' ./etc/xdg/templates/_root_.icewm_menu > /tmp/_root_.icewm_menu
-  mv -f /tmp/_root_.icewm_menu ./etc/xdg/templates/
- fi
- if [ "`find ./usr/local/bin ./usr/bin ./usr/sbin ./usr/X11R7/bin -maxdepth 1 -type f -name fluxbox`" = "" ];then
-  grep -v ' fluxbox$' ./etc/xdg/templates/_root_.icewm_menu > /tmp/_root_.icewm_menu
-  mv -f /tmp/_root_.icewm_menu ./etc/xdg/templates/
- fi
- if [ "`find ./usr/local/bin ./usr/bin ./usr/sbin ./usr/X11R7/bin -maxdepth 1 -type f -name fvwm95`" = "" ];then
-  grep -v ' fvwm95$' ./etc/xdg/templates/_root_.icewm_menu > /tmp/_root_.icewm_menu
-  mv -f /tmp/_root_.icewm_menu ./etc/xdg/templates/
- fi
- if [ "`find ./usr/local/bin ./usr/bin ./usr/sbin ./usr/X11R7/bin -maxdepth 1 -type f -name jwm`" = "" ];then
-  grep -v ' jwm$' ./etc/xdg/templates/_root_.icewm_menu > /tmp/_root_.icewm_menu
-  mv -f /tmp/_root_.icewm_menu ./etc/xdg/templates/
- fi
- if [ "`find ./usr/local/bin ./usr/bin ./usr/sbin ./usr/X11R7/bin -maxdepth 1 -type f -name pwm`" = "" ];then
-  grep -v ' pwm$' ./etc/xdg/templates/_root_.icewm_menu > /tmp/_root_.icewm_menu
-  mv -f /tmp/_root_.icewm_menu ./etc/xdg/templates/
- fi
- if [ "`find ./usr/local/bin ./usr/bin ./usr/sbin ./usr/X11R7/bin -maxdepth 1 -type f -name xfce4-session`" = "" ];then
-  grep -v ' xfce4-session$' ./etc/xdg/templates/_root_.icewm_menu > /tmp/_root_.icewm_menu
-  mv -f /tmp/_root_.icewm_menu ./etc/xdg/templates/
- fi
 fi
 
 #screenshot
 TAS=`find usr/bin usr/sbin usr/local/bin -name tas`
 SCREENY=`find usr/bin usr/sbin -name 'screeny'`
-if [ "$TAS" ];then echo '#!/bin/sh
-exec tas' > usr/local/bin/defaultscreenshot
-chmod 755 usr/local/bin/defaultscreenshot
-SCR=tas
-elif [ "$SCREENY" ];then echo '#!/bin/sh
-exec screeny' > usr/local/bin/defaultscreenshot
-chmod 755 usr/local/bin/defaultscreenshot
-SCR=screeny
+if [ "$TAS" ];then
+	SCR=tas
+elif [ "$SCREENY" ];then
+	SCR=screeny
 else
-echo '#!/bin/sh
-exec mtpaint -s' > usr/local/bin/defaultscreenshot
-chmod 755 usr/local/bin/defaultscreenshot
-SCR="mtpaint -s"
+	SCR="mtpaint -s"
 fi
+echo '#!/bin/sh
+exec '$SCR > usr/local/bin/defaultscreenshot
+chmod 755 usr/local/bin/defaultscreenshot
 echo "Setting $SCR as defaultscreenshot app"
 
-#end#
+### END ###
