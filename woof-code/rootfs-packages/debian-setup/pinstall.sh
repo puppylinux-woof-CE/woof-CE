f3304fcdd115da553c7f9ad2c68bb4a78b384c8e
james <james@localhost>
2019-05-12 02:26:35 +1000

    add support to be able to run SNS for wired network from ubuntu trusty.

--- woof-code/rootfs-packages/debian-setup/pinstall.sh
+++ woof-code/rootfs-packages/debian-setup/pinstall.sh
@@ -3,8 +3,13 @@
 #
 
 rm -f etc/init.d/udev                   # puppy already starts udev in rc.sysinit
+rm -f etc/init.d/dhcpcd                 # puppy already starts dhcpcd in sns
 rm usr/bin/X; ln -sf Xorg usr/bin/X     # delete useless xorg wrapper
 ln -s xterm usr/bin/x-terminal-emulator # some programs look for this symlink
+
+# symlink dhcpcd
+! [ -e sbin/dhcpcd ] && [ -e sbin/dhcpcd3 ] && ln -s dhcpcd3 sbin/dhcpcd
+! [ -e sbin/dhcpcd ] && [ -e sbin/dhcpcd5 ] && ln -s dhcpcd5 sbin/dhcpcd
 
 echo Generating locale files
 chroot . /usr/bin/localedef --no-archive -i en_US en_US
