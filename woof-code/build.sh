0f3cc6d454c2623eaaf60f8cc547d046822f3652
wdlkmpx <wdlkmpx@gmail.com>
2019-05-15 02:39:03 +0800

    woof-code/build.sh [n]
     build.sh [n]
       n=0: start from 0setup
       n=1: start from 1download
       n=2: start from 2createpackages
       n=3: start from 3buildistro-Z

--- woof-code/build.sh
+++ woof-code/build.sh
@@ -6,6 +6,12 @@
 #
 # think about who pays the server bills before running this
 #
+# build.sh [n]
+#   n=0: start from 0setup
+#   n=1: start from 1download
+#   n=2: start from 2createpackages
+#   n=3: start from 3buildistro-Z
+#
 
 if [ ! -f ./WOOFMERGEVARS ] ; then
 	echo "run merge2out"
@@ -14,7 +20,23 @@ fi
 
 # run helper scripts and log the output (script.log)
 
-./xlog 0setup a             #requires a param or ENTER the 2nd time you run it
-./xlog 1download            #[pkg]
-./xlog 2createpackages -all #or pkg
-./xlog 3builddistro-Z
+case $1 in
+0)
+	./xlog 0setup a             #requires a param or ENTER the 2nd time you run it
+	./xlog 1download            #[pkg]
+	./xlog 2createpackages -all #or pkg
+	./xlog 3builddistro-Z
+	;;
+1)
+	./xlog 1download            #[pkg]
+	./xlog 2createpackages -all #or pkg
+	./xlog 3builddistro-Z
+	;;
+2)
+	./xlog 2createpackages -all #or pkg
+	./xlog 3builddistro-Z
+	;;
+3)
+	./xlog 3builddistro-Z
+	;;
+esac
