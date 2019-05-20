44a8e1b75e7db5f8cc31d0e21df9a896a46d7ca4
wdlkmpx <wdlkmpx@gmail.com>
2019-05-21 06:30:58 +0800

    findwoofinstalledpkgs: add consistency into the mix

--- woof-code/support/builtin_files.sh
+++ woof-code/support/builtin_files.sh
@@ -2,49 +2,39 @@
 # * sourced by 3builddistro
 # * we're in sandbox3
 
-if [ "$BUILDSYS" != "yes" ] ; then
-	echo "* standalone *"
-	cd ..
-	. ./_00build.conf
-	. ./_00func
-	. ./DISTRO_SPECS
-	source_compat_repos
-	source_pkgs_specs
-	set_binaries_var
-	set_archdir_var
-	cd sandbox3
-fi
+. ../_00func
+. ../DISTRO_SPECS
 
-#======================================================================================
-
-#100527 build a .pet with lists of all builtin files...
-echo
-echo "Now building lists of all packages and files builtin to the SFS..."
+echo -e "\nNow building lists of all packages and files builtin to the SFS..."
 rm -rf 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION} 2>/dev/null
 
 mkdir 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}
 mkdir -p /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}
 mkdir -p 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}${PACKAGES_DIR}/builtin_files
 
-for ONEGENDIR in $(find -H ../packages-${DISTRO_FILE_PREFIX} -maxdepth 1 -type d | sort)
+cat ../status/findpkgs_FINAL_PKGS-${DISTRO_BINARY_COMPAT}-${DISTRO_COMPAT_VERSION} | sort | \
+while IFS="|" read -r F1 F2 F3 F4 F5 ETC
 do
-	ONEGENNAME=${ONEGENDIR##*/} #basename $ONEGENDIR
-	if ! ( echo "$PKGS_SPECS_TABLE" | grep -q "^yes|${ONEGENNAME}|" ) ; then
-		continue
-	fi
-	echo -n "$ONEGENNAME "
-	find -H $ONEGENDIR -type f -o -type l | \
-		sed -e "s%^\\.\\./packages-${DISTRO_FILE_PREFIX}/${ONEGENNAME}/%/%" | \
-		sort > /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/${ONEGENNAME}.files
-	sync
-	(
-	while read ONELINE
-	do
-		if [ -e "rootfs-complete${ONELINE}" ];then
-			echo "${ONELINE}" #only files that are in rootfs-complete
+	GENERICNAMES="$F1"
+	NAMEONLY="$F5"
+	case $NAMEONLY in *"_DOC"|*"_NLS"|*"_DEV") continue ;; esac
+	for i in ${GENERICNAMES//:/ } ; do
+		if [ -d ../packages-${DISTRO_FILE_PREFIX}/$i ] ; then
+			if [ -f 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}${PACKAGES_DIR}/builtin_files/${i} ] ; then
+				continue
+			fi
+			echo -n "$i "
+			find -H ../packages-${DISTRO_FILE_PREFIX}/$i -type f -o -type l | \
+				sed -e "s%^\\.\\./packages-${DISTRO_FILE_PREFIX}/${i}/%/%" | \
+				sort > /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/${i}.files
+			sync
+			(
+			while read ONELINE ; do
+				[ -e "rootfs-complete${ONELINE}" ] && echo "${ONELINE}" #only files that are in rootfs-complete
+			done < /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/${i}.files
+			) > 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}${PACKAGES_DIR}/builtin_files/${i}
 		fi
-	done < /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/${ONEGENNAME}.files
-	) > 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}${PACKAGES_DIR}/builtin_files/${ONEGENNAME}
+	done
 done
 
 # do the same for rootfs-packages
@@ -57,8 +47,7 @@ if [ -f /tmp/rootfs-packages.specs ];then
 			sed -e "s%^\\.\\./rootfs-packages/${PKGL}/%/%" | \
 			sort > /tmp/0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}/${PKGL}.files
 		sync
-		while read ONELINE
-		do
+		while read ONELINE ; do
 			if [ -e "rootfs-complete${ONELINE}" ];then
 				echo "${ONELINE}" >> 0builtin_files_${DISTRO_FILE_PREFIX}-${DISTRO_VERSION}${PACKAGES_DIR}/builtin_files/${PKGL}
 			fi
