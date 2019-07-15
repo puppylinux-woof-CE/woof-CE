fa37e3181ecb862b0457c6df466329cad529a25a
wdlkmpx <wdlkmpx@gmail.com>
2019-07-15 21:48:20 +0800

    gio_extra_modules.sh: add x86_64-linux-gnu plus misc edits

--- woof-code/rootfs-skeleton/etc/profile.d/gio_extra_modules.sh
+++ woof-code/rootfs-skeleton/etc/profile.d/gio_extra_modules.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 # not sourcing DISTRO_SPECS...
 
-xARCH="$(uname -m)"
+ZZZ_xARCH="$(uname -m)"
 
-if [ -f /usr/lib64/gio/modules/libdconfsettings.so ] && [ "$(echo "$xARCH" | grep "^.*64")" != "" ]; then
-	GIO_EXTRA_MODULES='/usr/lib64/gio/modules/'
-elif [ -f /usr/lib/i386-linux-gnu/gio/modules/libdconfsettings.so ] && [ "$(echo "$xARCH" | grep -E "^.*86")" != "" ]; then
+if [ -f /usr/lib/x86_64-linux-gnu/gio/modules/libdconfsettings.so ] && [ "$ZZZ_xARCH" = "x86_64" ]; then
+	GIO_EXTRA_MODULES='/usr/lib/x86_64-linux-gnu/gio/modules/'
+elif [ -f /usr/lib/i386-linux-gnu/gio/modules/libdconfsettings.so ] && [ "$(echo "$ZZZ_xARCH" | grep "i[3-6]86")" != "" ]; then
 	GIO_EXTRA_MODULES='/usr/lib/i386-linux-gnu/gio/modules/'
-elif [ -f /usr/lib/arm-linux-gnueabihf/gio/modules/libdconfsettings.so ] && [ "$(echo "$xARCH" | grep -E "^arm*")" != "" ]; then
+elif [ -f /usr/lib/arm-linux-gnueabihf/gio/modules/libdconfsettings.so ] && [ "$(echo "$ZZZ_xARCH" | grep "arm")" != "" ]; then
 	GIO_EXTRA_MODULES='/usr/lib/arm-linux-gnueabihf/gio/modules/'
-elif [ -f /usr/lib/aarch64-linux-gnu/gio/modules/libdconfsettings.so ] && [ "$(echo "$xARCH" | grep -E "aarch*")" != "" ]; then
+elif [ -f /usr/lib/aarch64-linux-gnu/gio/modules/libdconfsettings.so ] && [ "$ZZZ_xARCH" = "aarch64" ]; then
 	GIO_EXTRA_MODULES='/usr/lib/aarch64-linux-gnu/gio/modules/'
 else
-   if [ -f /usr/lib/gio/modules/libdconfsettings.so ] ; then
-     GIO_EXTRA_MODULES='/usr/lib/gio/modules/'
-   fi
+	if [ -f /usr/lib64/gio/modules/libdconfsettings.so ] ; then
+		GIO_EXTRA_MODULES='/usr/lib64/gio/modules/'
+	elif [ -f /usr/lib/gio/modules/libdconfsettings.so ] ; then
+		GIO_EXTRA_MODULES='/usr/lib/gio/modules/'
+	fi
 fi
 
 if [ "$GIO_EXTRA_MODULES" ] ; then
-  export GIO_EXTRA_MODULES
+	export GIO_EXTRA_MODULES
 fi
 
+unset ZZZ_xARCH
