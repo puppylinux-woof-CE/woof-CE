5c8511775df455bc28a47d39a9843d561e934b32
rizalmart <49640119+rizalmart@users.noreply.github.com>
2019-07-15 14:01:16 +0800

    Update gio_extra_modules.sh

--- woof-code/rootfs-skeleton/etc/profile.d/gio_extra_modules.sh
+++ woof-code/rootfs-skeleton/etc/profile.d/gio_extra_modules.sh
@@ -3,11 +3,11 @@
 
 if [ -f /usr/lib64/gio/modules/libdconfsettings.so ] && [ "$(uname -m | grep "^.*64")" != "" ]; then
 	GIO_EXTRA_MODULES='/usr/lib64/gio/modules/'
-elif [ -f /usr/lib/i386-linux-gnu/gio/modules/libdconfsettings.so ] && [ "$(uname -m | grep -E "^.*86")" == "" ]; then
+elif [ -f /usr/lib/i386-linux-gnu/gio/modules/libdconfsettings.so ] && [ "$(uname -m | grep -E "^.*86")" != "" ]; then
 	GIO_EXTRA_MODULES='/usr/lib/i386-linux-gnu/gio/modules/'
-elif [ -f /usr/lib/arm-linux-gnueabihf/gio/modules/libdconfsettings.so ] && [ "$(uname -m | grep -E "^arm*")" == "" ]; then
+elif [ -f /usr/lib/arm-linux-gnueabihf/gio/modules/libdconfsettings.so ] && [ "$(uname -m | grep -E "^arm*")" != "" ]; then
 	GIO_EXTRA_MODULES='/usr/lib/arm-linux-gnueabihf/gio/modules/'
-elif [ -f /usr/lib/aarch64-linux-gnu/gio/modules/libdconfsettings.so ] && [ "$(uname -m | grep -E "aarch*")" == "" ]; then
+elif [ -f /usr/lib/aarch64-linux-gnu/gio/modules/libdconfsettings.so ] && [ "$(uname -m | grep -E "aarch*")" != "" ]; then
 	GIO_EXTRA_MODULES='/usr/lib/aarch64-linux-gnu/gio/modules/'
 else
    if [ -f /usr/lib/gio/modules/libdconfsettings.so ] ; then
