7d894ee54c86537d4f0fe14cf1318b38d695f28f
rizalmart <49640119+rizalmart@users.noreply.github.com>
2019-09-18 14:29:56 +0800

    Update installpkg.sh

--- woof-code/rootfs-skeleton/usr/local/petget/installpkg.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/installpkg.sh
@@ -718,7 +718,7 @@ if [ "`grep "/lib/modules/$KERNVER/" $PKGFILES`" != "" ];then
  depmod -a
 fi
 
-if [ "`grep '/usr/share/icons/' $PKGFILES`" != "" ] && [ "$(cat "$PKGFILES" | grep "/mimetypes/")" != "" ]; then
+if [ "$(cat "$PKGFILES" | grep "/usr/share/icons/" | grep "/mimetypes/")" != "" ]; then
   if [ -e /usr/local/apps/ROX-Filer/ROX-Filer ] || [ -e /usr/bin/rox-filer ] || [ -e /usr/bin/rox ]; then
    [ "$(which sync-rox-icons)" != "" ] && sync-rox-icons
   fi
