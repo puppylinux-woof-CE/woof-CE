44a9cf72a8424ed709d2e82370f24436a209257a
wdlkmpx <wdlkmpx@gmail.com>
2019-10-01 06:35:51 +0800

    slacko 14/14.1: palemoon sfs (adrv) + cleanup
    it's a sfs file with a slightly optimized palemoon
    
    - initial page: blank page
    - cache up to 30mb of files
    - includes ublock origin (but it's disabled)
      * go to Tools menu -> Add-ons -> Extensions and enable it
    
      (don't know to enable it by default)

--- woof-code/rootfs-skeleton/pinstall.sh
+++ woof-code/rootfs-skeleton/pinstall.sh
@@ -58,4 +58,22 @@ exec '$SCR > usr/local/bin/defaultscreenshot
 chmod 755 usr/local/bin/defaultscreenshot
 echo "Setting $SCR as defaultscreenshot app"
 
+# if defaultbrowser = mozstart, it probably needs some automatic logic
+if grep -q 'mozstart' usr/local/bin/defaultbrowser ; then
+	DEFBROWSER=
+	if [ -f usr/bin/firefox ] ; then
+		DEFBROWSER=firefox
+	elif [ -f usr/bin/seamonkey ] ; then
+		DEFBROWSER=seamonkey
+	elif [ -f usr/bin/palemoon ] ; then
+		DEFBROWSER=palemoon
+	fi
+	if [ "$DEFBROWSER" ] ; then
+		echo "Setting $DEFBROWSER as a potentially default web browser"
+		echo '#!/bin/ash
+exec '${DEFBROWSER}' "$@"
+' > usr/local/bin/defaultbrowser
+	fi
+fi
+
 ### END ###
