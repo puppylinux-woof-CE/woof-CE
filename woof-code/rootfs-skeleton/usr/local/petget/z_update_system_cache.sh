44de3a89ddc7488859956390bb750d03ea912fb3
rizalmart <49640119+rizalmart@users.noreply.github.com>
2020-11-25 19:46:26 +0800

    z_update_system_cache.sh: Add small hacks
    Adding small hacks for pm-utils and ConsoleKit2
    
    pm-utils:
    * Redirect pm-hibernate and pm-suspend-hybrid to pm-suspend
    
    consolekit2:
    * Modify consolekit scripts to run shutdownconfig at PUPMODE 5

--- woof-code/rootfs-skeleton/usr/local/petget/z_update_system_cache.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/z_update_system_cache.sh
@@ -91,3 +91,97 @@ do
 	fi
   fi
 done
+
+#pm-utils hack
+if [ "$(cat "$PKGFILES" | grep "bin/pm-suspend-hybrid")" != "" ]; then
+ for pmsh in $(cat "$PKGFILES" | grep "bin/pm-suspend-hybrid")
+ do
+ rm -f $pmsh
+echo "#!/bin/sh
+ exec pm-suspend
+" > $pmsh
+  chmod +x $pmsh
+ done
+fi
+
+if [ "$(cat "$PKGFILES" | grep "bin/pm-hibernate")" != "" ]; then
+ for pmhib in $(cat "$PKGFILES" | grep "bin/pm-hibernate")
+ do
+ rm -f $pmhib
+echo "#!/bin/sh
+ exec pm-suspend
+" > $pmhib
+  chmod +x $pmhib
+ done
+fi
+
+#ConsoleKit2 hack
+if [ "$(cat "$PKGFILES" | grep "/ConsoleKit/scripts/ck-system-restart")" != "" ]; then
+ for flck in $(cat "$PKGFILES" | grep "/ConsoleKit/scripts/ck-system-restart")
+ do
+ rm -f $flck
+echo "#!/bin/sh
+
+shutdown_system(){
+
+ . /etc/rc.d/PUPSTATE
+
+ if [ $PUPMODE -eq 5 ]; then 
+	shutdownconfig
+ fi
+
+ $@
+ exit $?
+		
+}
+
+
+#Try for common tools
+if [ -x \"/sbin/shutdown\" ] ; then
+	shutdown_system \"/sbin/shutdown\" -r now
+elif [ -x \"/usr/sbin/shutdown\" ] ; then
+	shutdown_system \"/usr/sbin/shutdown\" -r now
+else
+	exit 1
+fi
+
+" > $flck
+  chmod +x $flck
+ done
+fi
+
+
+if [ "$(cat "$PKGFILES" | grep "/ConsoleKit/scripts/ck-system-stop")" != "" ]; then
+ for flck in $(cat "$PKGFILES" | grep "/ConsoleKit/scripts/ck-system-stop")
+ do
+ rm -f $flck
+echo "#!/bin/sh
+
+shutdown_system(){
+
+ . /etc/rc.d/PUPSTATE
+
+ if [ $PUPMODE -eq 5 ]; then 
+	shutdownconfig
+ fi
+
+ $@
+ exit $?
+		
+}
+
+
+#Try for common tools
+if [ -x \"/sbin/shutdown\" ] ; then
+	shutdown_system \"/sbin/shutdown\" -h now
+elif [ -x \"/usr/sbin/shutdown\" ] ; then
+	shutdown_system \"/usr/sbin/shutdown\" -h now
+else
+	exit 1
+fi
+
+" > $flck
+  chmod +x $flck
+ done
+fi
+
