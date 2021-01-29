933c49cf54a44507051b1c43e9646c0a9145dba3
Dima Krasner <dima@dimakrasner.com>
2021-01-29 15:48:07 +0200

    use a native ccache executable to speed up compilation

--- woof-code/support/petbuilds.sh
+++ woof-code/support/petbuilds.sh
@@ -8,15 +8,8 @@ fi
 
 [ -z "$WOOF_CXXFLAGS"] && WOOF_CXXFLAGS="$WOOF_CFLAGS"
 
-HAVE_CCACHE=0
-[ -e devx/usr/bin/ccache ] && HAVE_CCACHE=1
-
-WOOF_CC=gcc
-WOOF_CXX=g++
-if [ $HAVE_CCACHE -eq 1 ]; then
-    WOOF_CC="ccache gcc"
-    WOOF_CXX="ccache g++"
-fi
+WOOF_CC="/ccache gcc"
+WOOF_CXX="/ccache g++""
 
 WOOF_CFLAGS="$WOOF_CFLAGS -Os -fomit-frame-pointer -ffunction-sections -fdata-sections -fmerge-all-constants"
 WOOF_CXXCFLAGS="$WOOF_CXXCFLAGS -Os -fomit-frame-pointer -ffunction-sections -fdata-sections -fmerge-all-constants"
@@ -47,6 +40,19 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
 
             rm -f sh petbuild-rootfs-complete/bin/sh
             ln -s bash petbuild-rootfs-complete/bin/sh
+
+            # to speed up compilation, we build a static, native ccache executable
+            if [ ! -f ../../local-repositories/ccache/ccache ]; then
+                mkdir -p ../../local-repositories/ccache
+                wget -t 1 -T 15 -O ../../local-repositories/ccache/ccache-3.7.12.tar.xz https://github.com/ccache/ccache/releases/download/v3.7.12/ccache-3.7.12.tar.xz
+                tar -xJf ../../local-repositories/ccache/ccache-3.7.12.tar.xz
+                cd ccache-3.7.12
+                CFLAGS=-O3 LDFLAGS="-static -Wl,-s" ./configure
+                MAKEFLAGS="$MAKEFLAGS" make
+                mv ccache ../../local-repositories/ccache/ccache
+                cd ..
+            fi
+            install -m 755 ../../local-repositories/ccache/ccache petbuild-rootfs-complete/ccache
 
             HAVE_ROOTFS=1
         fi
@@ -88,21 +94,19 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
         mount -t aufs -o br=../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}:devx:petbuild-rootfs-complete petbuild petbuild-rootfs-complete-${NAME}
 
         mkdir -p petbuild-rootfs-complete-${NAME}/proc petbuild-rootfs-complete-${NAME}/sys petbuild-rootfs-complete-${NAME}/dev petbuild-rootfs-complete-${NAME}/tmp
-        [ $HAVE_CCACHE -eq 1 ] && mkdir -p petbuild-rootfs-complete-${NAME}/root/.ccache
+        mkdir -p petbuild-rootfs-complete-${NAME}/root/.ccache
         mount --bind /proc petbuild-rootfs-complete-${NAME}/proc
         mount --bind /sys petbuild-rootfs-complete-${NAME}/sys
         mount --bind /dev petbuild-rootfs-complete-${NAME}/dev
         mount -t tmpfs -o size=1G petbuild-tmp-${NAME} petbuild-rootfs-complete-${NAME}/tmp
-        if [ $HAVE_CCACHE -eq 1 ]; then
-            mkdir -p ../../local-repositories/${WOOF_TARGETARCH}/petbuilds-ccache-${NAME}
-            mount --bind ../../local-repositories/${WOOF_TARGETARCH}/petbuilds-ccache-${NAME} petbuild-rootfs-complete-${NAME}/root/.ccache
-        fi
+        mkdir -p ../../local-repositories/${WOOF_TARGETARCH}/petbuilds-ccache-${NAME}
+        mount --bind ../../local-repositories/${WOOF_TARGETARCH}/petbuilds-ccache-${NAME} petbuild-rootfs-complete-${NAME}/root/.ccache
 
         cp -a ../../local-repositories/sources/${NAME}/* petbuild-rootfs-complete-${NAME}/tmp/
         cp -a ../rootfs-petbuilds/${NAME}/* petbuild-rootfs-complete-${NAME}/tmp/
         CC="$WOOF_CC" CXX="$WOOF_CXX" CFLAGS="$WOOF_CFLAGS" CXXFLAGS="$WOOF_CXXFLAGS" LDFLAGS="$WOOF_LDFLAGS" MAKEFLAGS="$MAKEFLAGS" chroot petbuild-rootfs-complete-${NAME} sh -ec "cd /tmp && . ./petbuild && build"
         ret=$?
-        [ $HAVE_CCACHE -eq 1 ] && umount -l petbuild-rootfs-complete-${NAME}/root/.ccache
+        umount -l petbuild-rootfs-complete-${NAME}/root/.ccache
         umount -l petbuild-rootfs-complete-${NAME}/tmp
         umount -l petbuild-rootfs-complete-${NAME}/dev
         umount -l petbuild-rootfs-complete-${NAME}/sys
@@ -117,7 +121,7 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
             exit 1
         fi
 
-        [ $HAVE_CCACHE -eq 1 ] && rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/root/.ccache
+        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/root/.ccache
         rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/tmp ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/etc/ssl ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/etc/resolv.conf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/share/man ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/share/info ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/root/.wget-hsts ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/share/icons/hicolor/icon-theme.cache
         rmdir ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/* 2>/dev/null
         find ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH} -name '.wh*' -delete
