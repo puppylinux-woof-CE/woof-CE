832ff20d365d6d237777e09aafea7af0985db2d0
Dima Krasner <dima@dimakrasner.com>
2021-01-31 19:29:37 +0200

    change the exit code of successful kernel build to 0

--- kernel-kit/build.sh
+++ kernel-kit/build.sh
@@ -1036,6 +1036,6 @@ Output files:
 fi
 
 echo "Done!"
-[ -f /usr/share/sounds/2barks.au ] && aplay /usr/share/sounds/2barks.au
+[ ! -f /usr/share/sounds/2barks.au ] || aplay /usr/share/sounds/2barks.au
 
 ### END ###
