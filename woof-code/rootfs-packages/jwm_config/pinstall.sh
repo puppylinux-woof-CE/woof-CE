e496268531fb04b5e04de45bfbc9044fadf4abe7
Dima Krasner <dima@dimakrasner.com>
2021-02-16 20:04:15 +0200

    draw less when moving or resizing windows on ARM

--- woof-code/rootfs-packages/jwm_config/pinstall.sh
+++ woof-code/rootfs-packages/jwm_config/pinstall.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+. etc/DISTRO_SPECS
+
 rm -f root/.jwm/jwmrc-personal_old
+
+# windows are slow to move or resize if drawing is slow
+case "${DISTRO_TARGETARCH}" in
+    x86*) ;;
+    *) sed 's/>opaque</>outline</g' -i root/.jwm/jwmrc-personal root/.jwm/backup/jwmrc-personal ;;
+esac
