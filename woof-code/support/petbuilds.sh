833006e35eeca8ae9763afefc7e23822ff08402b
Dima Krasner <dima@dimakrasner.com>
2021-02-18 22:16:30 +0200

    do not use local-repositories for petbuild related files

--- woof-code/support/petbuilds.sh
+++ woof-code/support/petbuilds.sh
@@ -32,7 +32,7 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
     fi
 
     HASH=`cat ../DISTRO_PKGS_SPECS-${DISTRO_BINARY_COMPAT}-${DISTRO_COMPAT_VERSION} ../DISTRO_COMPAT_REPOS ../DISTRO_COMPAT_REPOS-${DISTRO_BINARY_COMPAT}-${DISTRO_COMPAT_VERSION} ../DISTRO_PET_REPOS $i/petbuild 2>/dev/null | md5sum | awk '{print $1}'`
-    if [ ! -d "../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}" ]; then
+    if [ ! -d "../petbuild-output/${NAME}-${HASH}" ]; then
         if [ $HAVE_ROOTFS -eq 0 ]; then
             echo "Preparing build environment"
             rm -rf petbuild-rootfs-complete
@@ -42,48 +42,46 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
             ln -s bash petbuild-rootfs-complete/bin/sh
 
             # to speed up compilation, we build a static, native ccache executable
-            if [ ! -f ../../local-repositories/ccache/ccache ]; then
-                mkdir -p ../../local-repositories/ccache
-                [ ! -f ../../local-repositories/ccache/ccache-3.7.12.tar.xz ] && wget -t 1 -T 15 -O ../../local-repositories/ccache/ccache-3.7.12.tar.xz https://github.com/ccache/ccache/releases/download/v3.7.12/ccache-3.7.12.tar.xz
-                tar -xJf ../../local-repositories/ccache/ccache-3.7.12.tar.xz
+            if [ ! -f ../petbuild-cache/ccache ]; then
+                wget -t 1 -T 15 -O https://github.com/ccache/ccache/releases/download/v3.7.12/ccache-3.7.12.tar.xz
+                tar -xJf ccache-3.7.12.tar.xz
                 cd ccache-3.7.12
                 CFLAGS=-O3 LDFLAGS="-static -Wl,-s" ./configure
                 MAKEFLAGS="$MAKEFLAGS" make
-                mv ccache ../../../local-repositories/ccache/ccache
+                install -D -m 755 ccache ../../petbuild-cache/ccache
                 cd ..
             fi
-            install -m 755 ../../local-repositories/ccache/ccache petbuild-rootfs-complete/ccache
+            install -m 755 ../petbuild-cache/ccache petbuild-rootfs-complete/ccache
 
             # speed configure scripts by using a native shell executable and a native busybox
             if [ "$WOOF_HOSTARCH" != "$WOOF_TARGETARCH" ]; then
-                if [ ! -f ../../local-repositories/bash/bash ]; then
-                    mkdir -p ../../local-repositories/bash
-                    [ ! -f ../../local-repositories/bash/bash-5.1.tar.gz ] && wget -t 1 -T 15 -O ../../local-repositories/bash/bash-5.1.tar.gz https://ftp.gnu.org/gnu/bash/bash-5.1.tar.gz
-                    tar -xzf ../../local-repositories/bash/bash-5.1.tar.gz
+                if [ ! -f ../petbuild-cache/bash ]; then
+                    wget -t 1 -T 15 https://ftp.gnu.org/gnu/bash/bash-5.1.tar.gz
+                    tar -xzf bash-5.1.tar.gz
                     cd bash-5.1
                     CFLAGS=-O3 LDFLAGS="-static -Wl,-s" ./configure --enable-minimal-config
                     MAKEFLAGS="$MAKEFLAGS" make bash
-                    mv bash ../../../local-repositories/bash/bash
+                    install -D -m 755 bash ../../petbuild-cache/bash
                     cd ..
                 fi
 
                 rm -f petbuild-rootfs-complete/bin/sh petbuild-rootfs-complete/bin/bash
-                install -m 755 ../../local-repositories/bash/bash petbuild-rootfs-complete/bin/bash
+                install -m 755 ../petbuild-cache/bash petbuild-rootfs-complete/bin/bash
                 ln -s bash petbuild-rootfs-complete/bin/sh
 
-                if [ ! -f ../../local-repositories/busybox/busybox ]; then
-                    mkdir -p ../../local-repositories/busybox
-                    [ ! -f ../../local-repositories/busybox/busybox-1.32.1.tar.bz2 ] && wget -t 1 -T 15 -O ../../local-repositories/busybox/busybox-1.32.1.tar.bz2 https://busybox.net/downloads/busybox-1.32.1.tar.bz2
-                    tar -xjf ../../local-repositories/busybox/busybox-1.32.1.tar.bz2
+                if [ ! -f ../petbuild-cache/busybox ]; then
+                    mkdir -p ../petbuild-cache/busybox
+                    wget -t 1 -T 15 https://busybox.net/downloads/busybox-1.32.1.tar.bz2
+                    tar -xjf busybox-1.32.1.tar.bz2
                     cp -f ../rootfs-petbuilds/busybox/DOTconfig busybox-1.32.1/.config
                     cd busybox-1.32.1
                     make CONFIG_STATIC=y
-                    mv busybox ../../../local-repositories/busybox/busybox
+                    install -D -m 755 busybox ../../petbuild-cache/busybox
                     cd ..
                 fi
 
                 rm -f petbuild-rootfs-complete/bin/busybox
-                install -m 755 ../../local-repositories/busybox/busybox petbuild-rootfs-complete/bin/busybox
+                install -m 755 ../petbuild-cache/busybox petbuild-rootfs-complete/bin/busybox
             fi
 
             HAVE_ROOTFS=1
@@ -91,8 +89,8 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
 
         if [ $HAVE_BUSYBOX -eq 0 -a "$NAME" != "busybox" ]; then
             if [ ! -f petbuild-rootfs-complete/bin/busybox ]; then
-                if [ -f ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/busybox-latest/bin/busybox ]; then # busybox petbuild
-                    install -D -m 755 ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/busybox-latest/bin/busybox petbuild-rootfs-complete/bin/busybox
+                if [ -f ../petbuild-output/busybox-latest/bin/busybox ]; then # busybox petbuild
+                    install -D -m 755 ../petbuild-output/busybox-latest/bin/busybox petbuild-rootfs-complete/bin/busybox
                 elif [ -f ../packages-${DISTRO_FILE_PREFIX}/busybox/bin/busybox ]; then # prebuilt busybox
                     install -D -m 755 ../packages-${DISTRO_FILE_PREFIX}/busybox/bin/busybox petbuild-rootfs-complete/bin/busybox
                 elif [ "$NAME" != "busybox" ]; then
@@ -106,12 +104,12 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
 
         echo "Downloading ${NAME}"
 
-        if [ -d ../../local-repositories/sources/${NAME} ]; then
-            rm -rf ../../local-repositories/sources/${NAME}/* 2>/dev/null
+        if [ -d ../petbuild-sources/${NAME} ]; then
+            rm -rf ../petbuild-sources/${NAME}/* 2>/dev/null
         else
-            mkdir -p ../../local-repositories/sources/${NAME}
+            mkdir -p ../petbuild-sources/${NAME}
         fi
-        cd ../../local-repositories/sources/${NAME}
+        cd ../petbuild-sources/${NAME}
         . ${HERE}/../rootfs-petbuilds/${NAME}/petbuild
         download
         if [ -f ${HERE}/../rootfs-petbuilds/${NAME}/sha256.sum ]; then
@@ -122,8 +120,8 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
 
         cd $HERE
 
-        mkdir -p ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH} petbuild-rootfs-complete-${NAME}
-        mount -t aufs -o br=../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}:devx:petbuild-rootfs-complete petbuild petbuild-rootfs-complete-${NAME}
+        mkdir -p ../petbuild-output/${NAME}-${HASH} petbuild-rootfs-complete-${NAME}
+        mount -t aufs -o br=../petbuild-output/${NAME}-${HASH}:devx:petbuild-rootfs-complete petbuild petbuild-rootfs-complete-${NAME}
 
         mkdir -p petbuild-rootfs-complete-${NAME}/proc petbuild-rootfs-complete-${NAME}/sys petbuild-rootfs-complete-${NAME}/dev petbuild-rootfs-complete-${NAME}/tmp
         mkdir -p petbuild-rootfs-complete-${NAME}/root/.ccache
@@ -131,10 +129,10 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
         mount --bind /sys petbuild-rootfs-complete-${NAME}/sys
         mount --bind /dev petbuild-rootfs-complete-${NAME}/dev
         mount -t tmpfs -o size=1G petbuild-tmp-${NAME} petbuild-rootfs-complete-${NAME}/tmp
-        mkdir -p ../../local-repositories/${WOOF_TARGETARCH}/petbuilds-ccache
-        mount --bind ../../local-repositories/${WOOF_TARGETARCH}/petbuilds-ccache petbuild-rootfs-complete-${NAME}/root/.ccache
+        mkdir -p ../petbuild-cache/.ccache
+        mount --bind ../petbuild-cache/.ccache petbuild-rootfs-complete-${NAME}/root/.ccache
 
-        cp -a ../../local-repositories/sources/${NAME}/* petbuild-rootfs-complete-${NAME}/tmp/
+        cp -a ../petbuild-sources/${NAME}/* petbuild-rootfs-complete-${NAME}/tmp/
         cp -a ../rootfs-petbuilds/${NAME}/* petbuild-rootfs-complete-${NAME}/tmp/
         CC="$WOOF_CC" CXX="$WOOF_CXX" CFLAGS="$WOOF_CFLAGS" CXXFLAGS="$WOOF_CXXFLAGS" LDFLAGS="$WOOF_LDFLAGS" MAKEFLAGS="$MAKEFLAGS" CCACHE_DIR=/root/.ccache CCACHE_NOHASHDIR=1 chroot petbuild-rootfs-complete-${NAME} sh -ec "cd /tmp && . ./petbuild && build"
         ret=$?
@@ -148,73 +146,73 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
 
         if [ $ret -ne 0 ]; then
             echo "ERROR: failed to build ${NAME}"
-            rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}
+            rm -rf ../petbuild-output/${NAME}-${HASH}
             rm -rf petbuild-rootfs-complete
             exit 1
         fi
 
-        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/root/.ccache
-        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/tmp
-        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/etc/ssl
-        rm -f ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/etc/resolv.conf
-        rm -f ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/root/.wget-hsts
+        rm -rf ../petbuild-output/${NAME}-${HASH}/root/.ccache
+        rm -rf ../petbuild-output/${NAME}-${HASH}/tmp
+        rm -rf ../petbuild-output/${NAME}-${HASH}/etc/ssl
+        rm -f ../petbuild-output/${NAME}-${HASH}/etc/resolv.conf
+        rm -f ../petbuild-output/${NAME}-${HASH}/root/.wget-hsts
 
-        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/share/man
-        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/share/info
-        rm -f ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/share/icons/hicolor/icon-theme.cache
-        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/lib/pkgconfig
-        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/lib/pkgconfig
-        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/share/pkgconfig
-        rm -rf ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/include
+        rm -rf ../petbuild-output/${NAME}-${HASH}/usr/share/man
+        rm -rf ../petbuild-output/${NAME}-${HASH}/usr/share/info
+        rm -f ../petbuild-output/${NAME}-${HASH}/usr/share/icons/hicolor/icon-theme.cache
+        rm -rf ../petbuild-output/${NAME}-${HASH}/lib/pkgconfig
+        rm -rf ../petbuild-output/${NAME}-${HASH}/usr/lib/pkgconfig
+        rm -rf ../petbuild-output/${NAME}-${HASH}/usr/share/pkgconfig
+        rm -rf ../petbuild-output/${NAME}-${HASH}/usr/include
 
-        find ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH} -name '.wh*' -delete
-        find ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH} -name '.git*' -delete
-        find ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH} -name '*.a' -delete
-        find ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH} -name '*.la' -delete
+        find ../petbuild-output/${NAME}-${HASH} -name '.wh*' -delete
+        find ../petbuild-output/${NAME}-${HASH} -name '.git*' -delete
+        find ../petbuild-output/${NAME}-${HASH} -name '*.a' -delete
+        find ../petbuild-output/${NAME}-${HASH} -name '*.la' -delete
 
         LIBDIRS="lib usr/lib"
         case $DISTRO_BINARY_COMPAT in
         slackware64) # in slacko64, we move all shared libraries to lib64
             for LIBDIR in $LIBDIRS; do
-                mkdir -p ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/${LIBDIR}64
-                for SO in `ls ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/${LIBDIR}/*.so* 2>/dev/null`; do
-                    mv -f $SO /../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/${LIBDIR}64/
+                mkdir -p ../petbuild-output/${NAME}-${HASH}/${LIBDIR}64
+                for SO in `ls ../petbuild-output/${NAME}-${HASH}/${LIBDIR}/*.so* 2>/dev/null`; do
+                    mv -f $SO /../petbuild-output/${NAME}-${HASH}/${LIBDIR}64/
                 done
             done
             ;;
 
         raspbian|debian|devuan|ubuntu|trisquel) # in debian, we move all shared libraries to ARCHDIR, e.g. lib/arm-linux-gnueabihf
             for LIBDIR in $LIBDIRS; do
-                mkdir -p ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/${LIBDIR}/${ARCHDIR}
-                for SO in `ls ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/${LIBDIR}/*.so* 2>/dev/null`; do
-                    mv -f $SO /../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/${LIBDIR}/${ARCHDIR}/
+                mkdir -p ../petbuild-output/${NAME}-${HASH}/${LIBDIR}/${ARCHDIR}
+                for SO in `ls ../petbuild-output/${NAME}-${HASH}/${LIBDIR}/*.so* 2>/dev/null`; do
+                    mv -f $SO /../petbuild-output/${NAME}-${HASH}/${LIBDIR}/${ARCHDIR}/
                 done
             done
             ;;
         esac
 
-        rmdir ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/share/* 2>/dev/null
-        rmdir ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/usr/* 2>/dev/null
-        rmdir ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/* 2>/dev/null
+        rmdir ../petbuild-output/${NAME}-${HASH}/usr/share/* 2>/dev/null
+        rmdir ../petbuild-output/${NAME}-${HASH}/usr/* 2>/dev/null
+        rmdir ../petbuild-output/${NAME}-${HASH}/* 2>/dev/null
 
-        find ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH} -type l | while read LINK; do
+        find ../petbuild-output/${NAME}-${HASH} -type l | while read LINK; do
             [ "`readlink $LINK`" = "/bin/busybox" ] && rm -f $LINK
         done
 
-        find ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH} -type f | while read ELF; do
+        find ../petbuild-output/${NAME}-${HASH} -type f | while read ELF; do
             strip --strip-all -R .note -R .comment ${ELF} 2>/dev/null
         done
 
         for EXTRAFILE in ../rootfs-petbuilds/${NAME}/*; do
             case "${EXTRAFILE##*/}" in
             petbuild|pinstall.sh|pet.specs|*.patch|sha256.sum|*-*|DOTconfig|*.c) ;;
-            *) cp -a $EXTRAFILE ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-${HASH}/
+            *) cp -a $EXTRAFILE ../petbuild-output/${NAME}-${HASH}/
             esac
         done
     fi
 
-    rm -f ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-latest
-    ln -s ${NAME}-${HASH} ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-latest
+    rm -f ../petbuild-output/${NAME}-latest
+    ln -s ${NAME}-${HASH} ../petbuild-output/${NAME}-latest
 
     PKGS="$PKGS $NAME"
 done
@@ -224,7 +222,7 @@ done
 for NAME in $PKGS; do
     echo "Copying ${NAME}"
 
-    cp -a ../../local-repositories/${WOOF_TARGETARCH}/petbuilds/${DISTRO_FILE_PREFIX}/${NAME}-latest/* rootfs-complete/
+    cp -a ../petbuild-output/${NAME}-latest/* rootfs-complete/
 
     if [ -f ../rootfs-petbuilds/${NAME}/pinstall.sh ]; then
         echo >> /tmp/rootfs_pkgs_pinstall.sh
