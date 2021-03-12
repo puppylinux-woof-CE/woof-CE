b2e59fd7c03bd96fcfb6a1dec5fa587df0d135d3
Dima Krasner <dima@dimakrasner.com>
2021-03-12 16:30:47 +0200

    Revert "run Firefox, Sylpheed and Transmission as spot"
    This reverts commit b1e5e63a0f2e4c14bd21f969d464185a644fb506.

--- woof-code/rootfs-petbuilds/sylpheed/pinstall.sh
+++ woof-code/rootfs-petbuilds/sylpheed/pinstall.sh
@@ -1,4 +1,2 @@
 echo '#!/bin/sh
 exec sylpheed "$@"' > usr/local/bin/defaultemail
-
-chroot . /usr/sbin/setup-spot sylpheed=true
--- woof-code/rootfs-petbuilds/transmission/pinstall.sh
+++ woof-code/rootfs-petbuilds/transmission/pinstall.sh
@@ -1,4 +1,2 @@
 echo '#!/bin/sh
 exec transmission-gtk "$@"' > usr/local/bin/defaulttorrent
-
-chroot . /usr/sbin/setup-spot transmission-gtk=true
