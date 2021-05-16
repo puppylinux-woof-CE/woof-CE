a15163940fa8baed2dffcb6d50ed1b13ae2dff50
Dima Krasner <dima@dimakrasner.com>
2021-05-16 08:25:18 +0300

    drawing is slow only when using Xorg

--- woof-code/rootfs-packages/jwm_config/pinstall.sh
+++ woof-code/rootfs-packages/jwm_config/pinstall.sh
@@ -5,7 +5,9 @@
 rm -f root/.jwm/jwmrc-personal_old
 
 # windows are slow to move or resize if drawing is slow
-case "${DISTRO_TARGETARCH}" in
-    x86*) ;;
-    *) sed 's/>opaque</>outline</g' -i root/.jwm/jwmrc-personal root/.jwm/backup/jwmrc-personal ;;
-esac
+if [ ! -f usr/bin/startxwayland ]; then
+    case "${DISTRO_TARGETARCH}" in
+        x86*) ;;
+        *) sed 's/>opaque</>outline</g' -i root/.jwm/jwmrc-personal root/.jwm/backup/jwmrc-personal ;;
+    esac
+fi
