10fc0de97cd5fdee001559bdb880356853168a56
Dima Krasner <dima@dimakrasner.com>
2021-06-12 09:42:29 +0300

    fix build failure of native busybox in ARM builds and fail next time this happens

--- woof-code/support/petbuilds.sh
+++ woof-code/support/petbuilds.sh
@@ -116,12 +116,12 @@ for i in ../rootfs-petbuilds/busybox ../rootfs-petbuilds/*; do
 
                 if [ ! -f ../petbuild-cache/busybox ]; then
                     mkdir -p ../petbuild-cache/busybox
-                    wget -t 1 -T 15 https://busybox.net/downloads/busybox-1.32.1.tar.bz2
-                    tar -xjf busybox-1.32.1.tar.bz2
-                    cp -f ../rootfs-petbuilds/busybox/DOTconfig busybox-1.32.1/.config
-                    cd busybox-1.32.1
+                    wget -t 1 -T 15 https://busybox.net/downloads/busybox-1.33.1.tar.bz2
+                    tar -xjf busybox-1.33.1.tar.bz2
+                    cp -f ../rootfs-petbuilds/busybox/DOTconfig busybox-1.33.1/.config
+                    cd busybox-1.33.1
                     make CONFIG_STATIC=y
-                    install -D -m 755 busybox ../../petbuild-cache/busybox
+                    install -D -m 755 busybox ../../petbuild-cache/busybox || exit 1
                     cd ..
                 fi
 
