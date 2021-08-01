3efad92bdad1daacd742d12a0422ae8c464df717
01micko <01micko@gmail.com>
2021-08-01 22:05:11 +1000

    fix bugs in support/get_icons.sh - re #2416 - get timestamps of downloaded icon packages and delete if 4 days old or more - better ouput messages - suppress sha256 output

--- woof-code/support/get_icons.sh
+++ woof-code/support/get_icons.sh
@@ -3,6 +3,12 @@
 ICONREPO=$1
 ICONSET=$2
 
+dld_icns() {
+	wget -t 3 -T 10 -q ${1} -P ../local-repositories/icons || return 1
+	wget -t 3 -T 10 -q ${1}.sha256.txt -P ../local-repositories/icons || return 1
+	return 0
+}
+
 [ -z "$ICONREPO" ] && exit 1
 [ -z "$ICONSET" ] && exit 1
 
@@ -17,17 +23,25 @@ DLD="${SITE}/${GUSER}/${ICONREPO}/releases/latest/download/$ICO_PKG"
 . ./DISTRO_SPECS
 mkdir -p ../local-repositories/icons
 mkdir -p ./icons-latest/${ICO_DIR}
-if ! [ -e "../local-repositories/icons/${ICO_PKG}" ] && ! [ -e "../local-repositories/icons/${ICO_PKG}.sha256.txt" ];then
-	wget -t 3 -T 10 -q $DLD -P ../local-repositories/icons
-	wget -t 3 -T 10 -q ${DLD}.sha256.txt -P ../local-repositories/icons
+if [ -e "../local-repositories/icons/${ICO_PKG}" ] && [ -e "../local-repositories/icons/${ICO_PKG}.sha256.txt" ];then
+	MTIME=$(date -r "../local-repositories/icons/${ICO_PKG}" "+%s")
+	CURTIME=$(date +%s)
+	DIFTIME=$(($CURTIME - $MTIME))
+	if [ $DIFTIME -gt 345600 ];then # 4 days = 345600 seconds
+		echo "deleting ${ICO_PKG} and re-downloading"
+		rm -f ../local-repositories/icons/${ICO_PKG}*
+		dld_icns $DLD
+	else
+		echo "${ICO_PKG} is still current"
+	fi
+else
+	dld_icns $DLD
 fi
 # extract to rootfs-complete
 [ -e "../local-repositories/icons/${ICO_PKG}" ] && \
-	sha256sum ../local-repositories/icons/${ICO_PKG}.sha256.txt || exit 1 
+	sha256sum ../local-repositories/icons/${ICO_PKG}.sha256.txt >/dev/null 2>&1 || exit 1 # suppress output
 tar xJf "../local-repositories/icons/${ICO_PKG}" -C ./icons-latest/${ICO_DIR} || exit 1
-(echo -n ":${ICO_DIR}:|pet|github|"; cat ./icons-latest/${ICO_DIR}/pet.specs) >> ./status/findpkgs_FINAL_PKGS-${DISTRO_BINARY_COMPAT}-${DISTRO_COMPAT_VERSION}
 echo -n "${ICO_DIR} " >> /tmp/icon-packages
-echo >> /tmp/icon-packages # newline
 rm -rf ./packages-${DISTRO_FILE_PREFIX}/${ICO_DIR}
 mv -f ./icons-latest/${ICO_DIR} ./packages-${DISTRO_FILE_PREFIX}/
 exit 0
