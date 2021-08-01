8b6ebc82627031ea4b76e9c0e78935762a638c4e
01micko <01micko@gmail.com>
2021-08-01 17:46:27 +1000

    update support/get_icons.sh

--- woof-code/support/get_icons.sh
+++ woof-code/support/get_icons.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
 ICONREPO=$1
+ICONSET=$2
 
 [ -z "$ICONREPO" ] && exit 1
+[ -z "$ICONSET" ] && exit 1
 
 SITE=https://github.com
 #GUSER=puppylinux-woof-CE
 GUSER=01micko
-RLSE=$(curl -s ${SITE}/${GUSER}/${ICONREPO}/releases | grep -o "\/${GUSER}.*\.tar\.xz" | head -n1) # gets latest
-ICO_PKG=${RLSE##*/}
+ICO_PKG=${ICONSET}.tar.xz
 ICO_DIR=${ICO_PKG%%\.*}
-[ -z "$RLSE" ] && exit 1 
-DLD="${SITE}${RLSE}"
+DLD="${SITE}/${GUSER}/${ICONREPO}/releases/latest/download/$ICO_PKG"
 
 # download
 . ./DISTRO_SPECS
 mkdir -p ../local-repositories/icons
-mkdir -p $MWD/icons-latest/${ICO_DIR}
-wget -t 1 -T 10 -q $DLD -P ../local-repositories/icons
-wget -t 1 -T 10 -q ${DLD}.sha256.txt -P ../local-repositories/icons
+mkdir -p ./icons-latest/${ICO_DIR}
+if ! [ -e "../local-repositories/icons/${ICO_PKG}" ] && ! [ -e "../local-repositories/icons/${ICO_PKG}.sha256.txt" ];then
+	wget -t 3 -T 10 -q $DLD -P ../local-repositories/icons
+	wget -t 3 -T 10 -q ${DLD}.sha256.txt -P ../local-repositories/icons
+fi
 # extract to rootfs-complete
 [ -e "../local-repositories/icons/${ICO_PKG}" ] && \
 	sha256sum ../local-repositories/icons/${ICO_PKG}.sha256.txt || exit 1 
@@ -28,5 +30,4 @@ echo -n "${ICO_DIR} " >> /tmp/icon-packages
 echo >> /tmp/icon-packages # newline
 rm -rf ./packages-${DISTRO_FILE_PREFIX}/${ICO_DIR}
 mv -f ./icons-latest/${ICO_DIR} ./packages-${DISTRO_FILE_PREFIX}/
-rm -f ../local-repositories/icons/${ICO_PKG}* # delete
 exit 0
