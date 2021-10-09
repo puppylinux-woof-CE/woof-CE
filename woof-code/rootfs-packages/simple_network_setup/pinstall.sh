0cc931d419e990cf96e0000a3483e02ccfa62962
rserwin1 <rerwin1@maine.rr.com>
2021-10-09 11:21:57 -0400

    SNS 3.2: reorganized, deprecate-free, enhanced upgrade

--- woof-code/rootfs-packages/simple_network_setup/pinstall.sh
+++ woof-code/rootfs-packages/simple_network_setup/pinstall.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
+
+# Replace placeholder for link to sns.
 ln -snf /usr/local/simple_network_setup/sns usr/local/bin/
-rm -f usr/sbin/sns #old link
+
+#Version 3.0:
+if [ "$(pwd)" = '/' ]; then
+    if [ -e usr/sbin/sns ];then
+        if [ ! -L usr/sbin/sns ] \
+          || [ "$(readlink usr/sbin/sns)" != "/usr/local/simple_network_setup/sns" ];then
+             mv usr/sbin/sns usr/sbin/sns-old.bak
+             ln -snf /usr/local/simple_network_setup/sns usr/sbin/
+        fi
+    fi
+
+    grep '^..[^:]' etc/simple_network_setup/connections \
+      && [ ! -f etc/simple_network_setup/connections-oldformat ] \
+      && mv etc/simple_network_setup/connections etc/simple_network_setup/connections-oldformat
+
+    if ! which iw >/dev/null 2>&1; then
+        BACK_TITLE="This version of simple-network-setup cannot manage wireless connections, due to the absence of the 'iw' command in this installation, but can control wired ethernet connections."
+        Xdialog --wmclass pgitprep --title "SNS - Barry's Simple Network Setup" --backtitle "$BACK_TITLE" --left --wrap --msgbox "If you need wireless support, either install 'iw' if available or uninstall this package and install or use a 2.4.x version of simple-network-setup." 0 70
+    fi
+fi
