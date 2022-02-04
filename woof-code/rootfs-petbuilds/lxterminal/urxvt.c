a7fd2ac15c8aac51ef5d9c008c4d3b76766c5be9
Dima Krasner <dima@dimakrasner.com>
2022-02-04 14:30:52 +0200

    fix breakage of simple_installer introduced in a1525d9

--- woof-code/rootfs-petbuilds/lxterminal/urxvt.c
+++ woof-code/rootfs-petbuilds/lxterminal/urxvt.c
@@ -12,8 +12,11 @@ int main(int argc, char **argv)
 		"-e"
 	};
 	gchar *cmd;
-	int i;
+	int i, j;
 	gboolean hold = FALSE;
+
+	if (argc >= 32)
+		goto proxy;
 
 	for (i = 1; i < argc; ++i) {
 		if ((strcmp(argv[i], "-hold") == 0) || (strcmp(argv[1], "--hold") == 0)) {
@@ -24,11 +27,10 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; ++i) {
 		if ((strcmp(argv[i], "-e") == 0) && (i < (argc - 1))) {
-			cmd = g_strjoinv(" ", &argv[i + 1]);
-
 			if (hold) {
 				new_argv[3] = "/bin/sh";
 				new_argv[4] = "-c";
+				cmd = g_strjoinv(" ", &argv[i + 1]);
 				new_argv[5] = g_strdup_printf("%s; echo; echo -n \"FINISHED. PRESS ENTER KEY TO CLOSE THIS WINDOW: \"; read simuldone", cmd);
 				g_free(cmd);
 				new_argv[6] = NULL;
@@ -37,16 +39,17 @@ int main(int argc, char **argv)
 				g_free(new_argv[5]);
 			}
 			else {
-				new_argv[3] = cmd;
+				for (++i, j = 3; i < argc; ++i, ++j)
+					new_argv[j] = argv[i];
 
 				execvp(new_argv[0], new_argv);
-				g_free(cmd);
 			}
 
 			return EXIT_FAILURE;
 		}
 	}
 
+proxy:
 	argv[0] = new_argv[0];
 	execvp(argv[0], argv);
 	return EXIT_FAILURE;
