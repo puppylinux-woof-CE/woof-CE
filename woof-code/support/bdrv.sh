645d476eb9203b43d4e148016c37c13344602506
Dima Krasner <dima@dimakrasner.com>
2022-02-19 20:47:50 +0800

    add Ubuntu bdrv support

--- woof-code/support/bdrv.sh
+++ woof-code/support/bdrv.sh
@@ -16,16 +16,23 @@ x86_64) ARCH=amd64 ;;
 x86) ARCH=i386  ;;
 arm) ARCH=armhf ;;
 arm64) ARCH=aarch64 ;;
+*) exit 1 ;;
+esac
+
+case "$DISTRO_BINARY_COMPAT" in
+debian) MIRROR=http://deb.debian.org/debian ;;
+ubuntu) MIRROR=http://archive.ubuntu.com/ubuntu ;;
+*) exit 1 ;;
 esac
 
 export LD_LIBRARY_PATH=
 export DEBIAN_FRONTEND=noninteractive
 
-# create a tiny Debian installation
+# create a tiny installation of the compatible distro
 if [ "$USR_SYMLINKS" = "yes" ]; then
-	$debootstrap --arch=$ARCH --variant=minbase ${DISTRO_COMPAT_VERSION} bdrv http://deb.debian.org/debian
+	$debootstrap --arch=$ARCH --variant=minbase ${DISTRO_COMPAT_VERSION} bdrv ${MIRROR}
 else
-	$debootstrap --no-merged-usr --arch=$ARCH --variant=minbase ${DISTRO_COMPAT_VERSION} bdrv http://deb.debian.org/debian
+	$debootstrap --no-merged-usr --arch=$ARCH --variant=minbase ${DISTRO_COMPAT_VERSION} bdrv ${MIRROR}
 fi
 
 # make sure UIDs and GIDs are consistent with Puppy
@@ -33,26 +40,45 @@ cat rootfs-complete/etc/group > bdrv/etc/group
 cat rootfs-complete/etc/passwd > bdrv/etc/passwd
 cat rootfs-complete/etc/shadow > bdrv/etc/shadow
 
-# blacklist packages that may conflict with packages in the main SFS
-chroot bdrv apt-mark hold busybox
-chroot bdrv apt-mark hold busybox-static
-
 mount --bind /etc/resolv.conf bdrv/etc/resolv.conf
 trap "umount -l bdrv/etc/resolv.conf" INT ERR
 
 # configure the package manager
-echo "deb http://deb.debian.org/debian ${DISTRO_COMPAT_VERSION} main contrib non-free" > bdrv/etc/apt/sources.list
-if [ "$DISTRO_COMPAT_VERSION" != "sid" ]; then
-	cat << EOF >> bdrv/etc/apt/sources.list
-deb http://deb.debian.org/debian ${DISTRO_COMPAT_VERSION}-updates main contrib non-free
-deb http://deb.debian.org/debian-security ${DISTRO_COMPAT_VERSION}-security main contrib non-free
+case "$DISTRO_BINARY_COMPAT" in
+debian)
+	echo "deb ${MIRROR} ${DISTRO_COMPAT_VERSION} main contrib non-free" > bdrv/etc/apt/sources.list
+
+	if [ "$DISTRO_COMPAT_VERSION" != "sid" ]; then
+		cat << EOF >> bdrv/etc/apt/sources.list
+deb ${MIRROR} ${DISTRO_COMPAT_VERSION}-updates main contrib non-free
+deb ${MIRROR}-security ${DISTRO_COMPAT_VERSION}-security main contrib non-free
 EOF
-fi
+	fi
+	;;
+
+ubuntu)
+	echo "deb ${MIRROR} ${DISTRO_COMPAT_VERSION} main universe multiverse restricted" > bdrv/etc/apt/sources.list
+
+	if [ "$DISTRO_COMPAT_VERSION" != "devel" ]; then
+		cat << EOF >> bdrv/etc/apt/sources.list
+deb ${MIRROR} ${DISTRO_COMPAT_VERSION}-updates main universe multiverse restricted
+deb ${MIRROR} ${DISTRO_COMPAT_VERSION}-security main universe multiverse restricted
+EOF
+	fi
+
+	# the x86 repo is small, contains popular packages like Steam and won't make apt update 2x slower
+	[ "$ARCH" != "amd64" ] || chroot bdrv dpkg --add-architecture i386
+	;;
+esac
 cat << EOF > bdrv/etc/apt/apt.conf.d/00puppy
 APT::Install-Recommends "false";
 APT::Install-Suggests "false";
 EOF
 chroot bdrv apt-get update
+
+# blacklist packages that may conflict with packages in the main SFS
+chroot bdrv apt-mark hold busybox
+chroot bdrv apt-mark hold busybox-static
 
 # install all packages included in the woof-CE build
 chroot bdrv apt-get install -y `cat ../status/findpkgs_FINAL_PKGS-${DISTRO_BINARY_COMPAT}-${DISTRO_COMPAT_VERSION} | cut -f 5 -d \| | tr '\n' ' '`
