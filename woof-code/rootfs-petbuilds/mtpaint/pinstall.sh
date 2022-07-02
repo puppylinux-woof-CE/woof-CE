1d7e6c1d71cfb4846db8d5ea3cf4213cc6ae9464
Dima Krasner <dima@dimakrasner.com>
2022-07-02 06:37:39 +0000

    add a wrapper around grim and mtpaint

--- woof-code/rootfs-petbuilds/mtpaint/pinstall.sh
+++ woof-code/rootfs-petbuilds/mtpaint/pinstall.sh
@@ -3,5 +3,5 @@ exec mtpaint "$@"' > usr/local/bin/defaultimageeditor
 chmod 755 usr/local/bin/defaultimageeditor
 
 echo '#!/bin/sh
-exec mtpaint -s "$@"' > usr/local/bin/defaultscreenshot
+exec mtpaint-screenshot "$@"' > usr/local/bin/defaultscreenshot
 chmod 755 usr/local/bin/defaultscreenshot
