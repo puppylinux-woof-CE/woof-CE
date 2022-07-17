8a83b6f22e55e7907ea7221e8ff9b1a84a9cae1c
Dima Krasner <dima@dimakrasner.com>
2022-07-17 15:17:42 +0300

    add support for dwl themes

--- woof-code/rootfs-petbuilds/dwl-kiosk/config.h
+++ woof-code/rootfs-petbuilds/dwl-kiosk/config.h
@@ -1,10 +1,10 @@
 /* appearance */
 static const int sloppyfocus        = 0;  /* focus follows mouse */
-static const unsigned int borderpx  = 0;  /* border pixel of windows */
+static unsigned int borderpx  = 1;  /* border pixel of windows */
 static const int lockfullscreen     = 1;  /* 1 will force focus on the fullscreen window */
-static const float rootcolor[]      = {0.3, 0.3, 0.3, 1.0};
-static const float bordercolor[]    = {0.5, 0.5, 0.5, 1.0};
-static const float focuscolor[]     = {1.0, 0.0, 0.0, 1.0};
+static float rootcolor[]      = {0.3, 0.3, 0.3, 1.0};
+static float bordercolor[]    = {0.5, 0.5, 0.5, 1.0};
+static float focuscolor[]     = {1.0, 0.0, 0.0, 1.0};
 
 /* pointer constraints */
 static const int allow_constrain      = 1;
