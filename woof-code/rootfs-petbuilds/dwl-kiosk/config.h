00e91289f41350cc2d3955690e72c6784248fcb3
Dima Krasner <dima@dimakrasner.com>
2022-08-28 09:53:55 +0000

    automatically set the dwl scale factor

--- woof-code/rootfs-petbuilds/dwl-kiosk/config.h
+++ woof-code/rootfs-petbuilds/dwl-kiosk/config.h
@@ -38,7 +38,7 @@ static const MonitorRule monrules[] = {
 	{ "eDP-1",    0.5,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL },
 	*/
 	/* defaults */
-	{ NULL,       0.64, 1,      1,    &layouts[3], WL_OUTPUT_TRANSFORM_NORMAL },
+	{ NULL,       0.64, 1,     -1,    &layouts[3], WL_OUTPUT_TRANSFORM_NORMAL },
 };
 
 /* keyboard */
