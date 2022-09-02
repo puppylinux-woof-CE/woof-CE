9963209ff73558c05c099ff86a570eb191ae19c9
Dima Krasner <dima@dimakrasner.com>
2022-09-02 11:52:25 +0000

    fix breakage of get_sof.sh since v2.2.1

--- kernel-kit/get_sof.sh
+++ kernel-kit/get_sof.sh
@@ -3,10 +3,14 @@
 [ -d sof-bin ] || git clone https://github.com/thesofproject/sof-bin
 cd sof-bin
 ver=`git describe --abbrev=0 --tags`
+git checkout $ver
 mkdir -p "$1/lib/firmware/intel"
-cp -r $ver*/sof-$ver "$1/lib/firmware/intel/"
+verfile=`ls */$ver`
+[ -n "$verfile" ] || exit 1
+verdir=`dirname $verfile`
+cp -r $verdir/sof/ "$1/lib/firmware/intel/sof-$ver"
 ln -s sof-$ver "$1/lib/firmware/intel/sof"
-cp -r $ver*/sof-tplg-$ver "$1/lib/firmware/intel/"
+cp -r $verdir/sof-tplg/ "$1/lib/firmware/intel/sof-tplg-$ver"
 ln -s sof-tplg-$ver "$1/lib/firmware/intel/sof-tplg"
 mkdir -p "$1/usr/share/doc/sof-bin"
 cp -f LICENCE.* Notice.* "$1/usr/share/doc/sof-bin/"
