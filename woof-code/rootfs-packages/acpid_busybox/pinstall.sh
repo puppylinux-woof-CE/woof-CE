76720ac0aaa0dc2d3d1217916353e896aedfc387
Dima Krasner <dima@dimakrasner.com>
2022-09-04 06:39:08 +0000

    use WAYLAND_DISPLAY to detect graphical sessions

--- woof-code/rootfs-packages/acpid_busybox/pinstall.sh
+++ woof-code/rootfs-packages/acpid_busybox/pinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ "$(pwd)" = "/" ] && [ -x /etc/init.d/rc.acpi ];then
-  export DISPLAY=""	# to simurate startup from rc.sysinit without X
+  export DISPLAY="" WAYLAND_DISPLAY=""	# to simurate startup from rc.sysinit without X
   /etc/init.d/rc.acpi restart
 fi
