e8f06965c5129d49368e2825ed39aa19fb08d288
Pieter Steyn <pieters@ringier.co.za>
2022-09-10 23:10:45 +0200

    Try set wallpaper.

--- woof-code/rootfs-skeleton/etc/init.d/90ccs.sh
+++ woof-code/rootfs-skeleton/etc/init.d/90ccs.sh
@@ -5,3 +5,14 @@ mkdir -p /root/.config/jami
 cp /etc/ccs/dring.yml  /root/.config/jami/
 echo "Overwriting jwm tray"
 cp /etc/ccs/jwm* /root/.jwm/
+echo "Setting wallpaper"
+cat << EOF > /root/.jwm/jwmrc-wallpaper
+<?xml version="1.0"?>
+
+<JWM>
+
+<Desktops>
+  <Background type="image">/usr/share/bliss.svg</Background>
+</Desktops>
+</JWM>
+EOF
