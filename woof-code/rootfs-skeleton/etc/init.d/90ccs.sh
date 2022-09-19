b074b333f99975205babb335f83177b0cade26b5
Pieter Steyn <pieters@ringier.co.za>
2022-09-19 08:55:26 +0200

    Try undo theme

--- woof-code/rootfs-skeleton/etc/init.d/90ccs.sh
+++ woof-code/rootfs-skeleton/etc/init.d/90ccs.sh
@@ -5,5 +5,5 @@ mkdir -p /root/.config/jami
 cp /etc/ccs/dring.yml  /root/.config/jami/
 echo "Overwriting jwm tray"
 cp /etc/ccs/jwm* /root/.jwm/
-echo "Setting wallpaper"
-set_wallpaper /usr/share/bliss.svg
+#echo "Setting wallpaper"
+#set_wallpaper /usr/share/bliss.svg
