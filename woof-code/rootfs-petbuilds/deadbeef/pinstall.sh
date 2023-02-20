3e77c12b974fc6409238ea27285f272abe723aa5
Dima Krasner <dima@dimakrasner.com>
2023-02-20 09:38:46 +0200

    set defaultcdplayer when using the deadbeef petbuild

--- woof-code/rootfs-petbuilds/deadbeef/pinstall.sh
+++ woof-code/rootfs-petbuilds/deadbeef/pinstall.sh
@@ -1,3 +1,7 @@
 echo '#!/bin/sh
 exec deadbeef "$@"' > usr/local/bin/defaultaudioplayer
 chmod 755 usr/local/bin/defaultaudioplayer
+
+echo '#!/bin/sh
+exec deadbeef all.cda' > usr/local/bin/defaultcdplayer
+chmod 755 usr/local/bin/defaultcdplayer
