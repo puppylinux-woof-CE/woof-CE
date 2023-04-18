692fa4e3de5cf3c0a65cb05178724073702f544e
Dima Krasner <dima@dimakrasner.com>
2023-04-18 19:09:03 +0300

    change the spacefm handler for .bmp images from viewnior to mtpaint

--- woof-code/rootfs-petbuilds/viewnior/pinstall.sh
+++ woof-code/rootfs-petbuilds/viewnior/pinstall.sh
@@ -4,6 +4,7 @@ chmod 755 usr/local/bin/defaultimageviewer
 
 if [ ! -e usr/local/apps/ROX-Filer/AppRun ]; then
 	for T in `grep ^MimeType= usr/share/applications/viewnior.desktop | sed -e s/^MimeType=//g -e 's/;/ /g'`; do
+		[ "$T" = "image/bmp" ] && continue
 		chroot . xdg-mime default viewnior.desktop $T
 		chroot . run-as-spot xdg-mime default viewnior.desktop $T
 	done
