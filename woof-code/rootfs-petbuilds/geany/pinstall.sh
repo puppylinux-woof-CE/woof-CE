071427937ead4f2374cb5bac4eecdc6634ef55a9
Dima Krasner <dima@dimakrasner.com>
2023-04-18 10:03:09 +0300

    use Geany for all text files

--- woof-code/rootfs-petbuilds/geany/pinstall.sh
+++ woof-code/rootfs-petbuilds/geany/pinstall.sh
@@ -5,3 +5,11 @@ chmod 755 usr/local/bin/defaulttexteditor
 echo '#!/bin/sh
 exec geany "$@"' > usr/local/bin/defaulthtmleditor
 chmod 755 usr/local/bin/defaulthtmleditor
+
+if [ ! -e usr/local/apps/ROX-Filer/AppRun ]; then
+	for T in `grep ^MimeType= usr/share/applications/geany.desktop | sed -e s/^MimeType=//g -e 's/;/ /g'`; do
+		[ "$T" = "text/plain" ] && continue
+		chroot . xdg-mime default geany.desktop $T
+		chroot . run-as-spot xdg-mime default geany.desktop $T
+	done
+fi
