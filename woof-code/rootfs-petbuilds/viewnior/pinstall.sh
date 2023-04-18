76709758a2c6da0ec6576d5cfcdbe503c6160c08
Dima Krasner <dima@dimakrasner.com>
2023-04-18 10:19:44 +0300

    set spacefm default file associations

--- woof-code/rootfs-petbuilds/viewnior/pinstall.sh
+++ woof-code/rootfs-petbuilds/viewnior/pinstall.sh
@@ -1,3 +1,10 @@
 echo '#!/bin/sh
 exec viewnior "$@"' > usr/local/bin/defaultimageviewer
 chmod 755 usr/local/bin/defaultimageviewer
+
+if [ ! -e usr/local/apps/ROX-Filer/AppRun ]; then
+	for T in `grep ^MimeType= usr/share/applications/viewnior.desktop | sed -e s/^MimeType=//g -e 's/;/ /g'`; do
+		chroot . xdg-mime default viewnior.desktop $T
+		chroot . run-as-spot xdg-mime default viewnior.desktop $T
+	done
+fi
