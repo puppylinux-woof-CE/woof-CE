169c885e21f34b80e3bbcc51c810172092e4a294
Dima Krasner <dima@dimakrasner.com>
2023-04-18 10:09:01 +0300

    use Geany for HTML files

--- woof-code/rootfs-petbuilds/geany/pinstall.sh
+++ woof-code/rootfs-petbuilds/geany/pinstall.sh
@@ -12,4 +12,6 @@ if [ ! -e usr/local/apps/ROX-Filer/AppRun ]; then
 		chroot . xdg-mime default geany.desktop $T
 		chroot . run-as-spot xdg-mime default geany.desktop $T
 	done
+	chroot . xdg-mime default geany.desktop "application/xhtml+xml"
+	chroot . run-as-spot xdg-mime default geany.desktop "application/xhtml+xml"
 fi
