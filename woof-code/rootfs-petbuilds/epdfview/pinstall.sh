8fdcfa9349a6cf39726a99d35103f72d1a77d1e7
Dima Krasner <dima@dimakrasner.com>
2023-07-09 08:55:50 +0300

    set epdfview as the default PDF handler under spacefm

--- woof-code/rootfs-petbuilds/epdfview/pinstall.sh
+++ woof-code/rootfs-petbuilds/epdfview/pinstall.sh
@@ -1,3 +1,8 @@
 echo '#!/bin/sh
 exec epdfview "$@"' > usr/local/bin/defaultpdfviewer
 chmod 755 usr/local/bin/defaultpdfviewer
+
+if [ ! -e usr/local/apps/ROX-Filer/AppRun ]; then
+	chroot . xdg-mime default epdfview.desktop application/pdf
+	chroot . run-as-spot xdg-mime default epdfview.desktop application/pdf
+fi
