254b1d017055c214e5fea6e2ab67859a946898d4
Richard Erwin <rerwin1@maine.rr.com>
2024-06-18 01:57:55 -0400

    Legacy network managers: Accommodate ConnMan, PeasyWiFi, gtk3, plus refinements (#4296)
    * Network Wizard 3.0.2
    
    * Frisbee 2.0.4
    
    * Simple Network Setup 3.5.2
    
    * Update sns - fix history comment typo
    
    * Update rc.snsnetwork - Removed incorrect history comment
    
    * Update rc.snsnetwork - correct another history comment

--- woof-code/rootfs-packages/simple_network_setup/puninstall.sh
+++ woof-code/rootfs-packages/simple_network_setup/puninstall.sh
@@ -4,3 +4,7 @@ if [ -e /usr/sbin/sns-old.bak ];then
     mv -f /usr/sbin/sns-old.bak /usr/sbin/sns
     chmod +x
 fi
+
+#v3.4 Restore old udev rules file.
+[ -e /initrd/pup_ro2/etc/udev/rules.d/51-simple_network_setup.rules ] \
+  && cp -n /initrd/pup_ro2/etc/udev/rules.d/51-simple_network_setup.rules /etc/udev/rules.d/
