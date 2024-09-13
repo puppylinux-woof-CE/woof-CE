9135162b280f12ff976e0f8e8b6aba5a60d3b1a7
PB <peabee@users.noreply.github.com>
2024-09-13 18:11:43 +0100

    Update devx-hack.sh

--- woof-distro/x86_64/debian/trixie64/support/gcc14_permissive/devx-hack.sh
+++ woof-distro/x86_64/debian/trixie64/support/gcc14_permissive/devx-hack.sh
@@ -1,3 +1,4 @@
 echo "For trixie devx: gcc14 permissive"
 cp -vf ./support/gcc14_permissive/gcc ./sandbox3/devx/usr/bin/
+chmod +x ./sandbox3/devx/usr/bin/gcc
 ln -svf /usr/bin/gcc ./sandbox3/devx/usr/bin/cc
