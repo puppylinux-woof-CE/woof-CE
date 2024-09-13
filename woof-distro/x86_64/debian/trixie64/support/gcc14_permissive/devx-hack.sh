724f4d67a86861a8ac9f7740803d42c10135fc2f
PB <peabee@users.noreply.github.com>
2024-09-13 17:37:45 +0100

    correct error devx-hack.sh

--- woof-distro/x86_64/debian/trixie64/support/gcc14_permissive/devx-hack.sh
+++ woof-distro/x86_64/debian/trixie64/support/gcc14_permissive/devx-hack.sh
@@ -1,3 +1,3 @@
 echo "For trixie devx: gcc14 permissive"
 cp -vf ./support/gcc14_permissive/gcc ./sandbox3/devx/usr/bin/
-ln -vf /usr/bin/gcc ./sandbox3/devx/usr/bin/cc
+ln -svf /usr/bin/gcc ./sandbox3/devx/usr/bin/cc
