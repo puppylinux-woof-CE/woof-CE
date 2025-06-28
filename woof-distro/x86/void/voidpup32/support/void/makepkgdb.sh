9be46e2cbda4932c28c72040e56117572b26b108
PB <peabee@users.noreply.github.com>
2025-06-28 11:33:03 +0100

    Update makepkgdb.sh cater for sourcepkg 3

--- woof-distro/x86/void/voidpup32/support/void/makepkgdb.sh
+++ woof-distro/x86/void/voidpup32/support/void/makepkgdb.sh
@@ -24,8 +24,10 @@ do
   sha256=`[ -e ${meta} ] && sha256sum ${meta} | cut -d ' ' -f 1`
   vpkg="<key>${vpkg}</key>"
   begnum=`grep -n $vpkg $repo | head -1 | cut -d : -f 1`
-  finnum=`tail -n +"$begnum" $repo | grep -n "</dict>" | head -1 | cut -d : -f 1`
-  finnum=`expr $begnum + $finnum - 1`
+  mednum=`tail -n +"$begnum" $repo | grep -n "<key>source" | head -1 | cut -d : -f 1`
+  mednum=`expr $begnum + $mednum - 1`
+  finnum=`tail -n +"$mednum" $repo | grep -n "</dict>" | head -1 | cut -d : -f 1`
+  finnum=`expr $mednum + $finnum - 1`
 
   sed -n "${begnum},${finnum}p" $repo > tmpout
   sed -i '/<key>filename-sha256/i \\t\t<key>automatic-install</key>\n\t\t<true/>' tmpout
