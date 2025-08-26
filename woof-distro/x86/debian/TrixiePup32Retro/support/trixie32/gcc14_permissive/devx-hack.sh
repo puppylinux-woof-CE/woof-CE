b94237f4253dd367a7756d5e23724a1e5727bc93
PB <peabee@users.noreply.github.com>
2025-08-26 11:47:20 +0100

    Delete woof-distro/x86/debian/TrixiePup32Retro directory

--- woof-distro/x86/debian/TrixiePup32Retro/support/trixie32/gcc14_permissive/devx-hack.sh
+++
@@ -1,5 +0,0 @@
-echo "For trixie devx: gcc14 permissive"
-rm ./sandbox3/rootfs-complete/usr/bin/gcc
-cp -vf ./support/trixie32/gcc14_permissive/gcc ./sandbox3/rootfs-complete/usr/bin/
-chmod +x ./sandbox3/rootfs-complete/usr/bin/gcc
-ln -svf /usr/bin/gcc ./sandbox3/rootfs-complete/usr/bin/cc
