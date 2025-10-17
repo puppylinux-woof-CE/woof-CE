fd8104f381cb07e2664450879d333fa56f6389d9
PB <peabee@users.noreply.github.com>
2025-10-17 09:41:45 +0100

    Update bdrv.sh modernize-sources

--- woof-code/support/bdrv.sh
+++ woof-code/support/bdrv.sh
@@ -130,7 +130,7 @@ APT::Install-Suggests "false";
 APT::Autoremove::SuggestsImportant "false";
 EOF
 chroot bdrv apt-get update
-#chroot bdrv apt modernize-sources -y
+chroot bdrv apt modernize-sources -y	#not supported in apt < 2.9.26
 chroot bdrv apt-get upgrade -y
 
 # blacklist packages that may conflict with packages in the main SFS
