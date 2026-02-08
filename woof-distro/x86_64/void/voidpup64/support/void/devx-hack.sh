4b8a5574b256182dce1043c787fef5b58e86b0e2
PB <peabee@users.noreply.github.com>
2026-02-08 14:54:54 +0000

    Update devx-hack.sh fix libtoolize

--- woof-distro/x86_64/void/voidpup64/support/void/devx-hack.sh
+++ woof-distro/x86_64/void/voidpup64/support/void/devx-hack.sh
@@ -3,4 +3,9 @@ cd ./sandbox3/devx
 cp -r usr/lib64/* usr/lib/
 rm -rf usr/lib64
 ln -s lib usr/lib64
+
+echo "Fix libtoolize"
+mkdir -p bin
+ln -s /usr/bin/sed bin/sed
+sed -i 's%#! /usr/bin/env sh%#!/bin/sh%' usr/bin/libtoolize
 cd ../../
