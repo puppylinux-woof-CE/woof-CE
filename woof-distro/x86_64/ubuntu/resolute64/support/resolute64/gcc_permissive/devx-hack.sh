b8d4cdb81afa3107bf779c7846d6d98d6298d6a9
PB <peabee@users.noreply.github.com>
2026-03-09 10:54:58 +0000

    Update devx-hack.sh

--- woof-distro/x86_64/ubuntu/resolute64/support/resolute64/gcc_permissive/devx-hack.sh
+++ woof-distro/x86_64/ubuntu/resolute64/support/resolute64/gcc_permissive/devx-hack.sh
@@ -1,4 +1,4 @@
 echo "For resolute devx: gcc permissive"
-cp -vf ./support/gcc_permissive/gcc ./sandbox3/devx/usr/bin/
+cp -vf ./support/resolute64/gcc_permissive/gcc ./sandbox3/devx/usr/bin/
 chmod +x ./sandbox3/devx/usr/bin/gcc
 ln -svf /usr/bin/gcc ./sandbox3/devx/usr/bin/cc
