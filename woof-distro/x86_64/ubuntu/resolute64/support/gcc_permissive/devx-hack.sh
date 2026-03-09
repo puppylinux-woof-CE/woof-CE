1ce8febf93d3e1c2e3ae3f01225a37bb302732ea
PB <peabee@users.noreply.github.com>
2026-03-09 09:47:59 +0000

    Delete woof-distro/x86_64/ubuntu/resolute64/support/gcc_permissive directory

--- woof-distro/x86_64/ubuntu/resolute64/support/gcc_permissive/devx-hack.sh
+++
@@ -1,4 +0,0 @@
-echo "For resolute devx: gcc permissive"
-cp -vf ./support/gcc_permissive/gcc ./sandbox3/devx/usr/bin/
-chmod +x ./sandbox3/devx/usr/bin/gcc
-ln -svf /usr/bin/gcc ./sandbox3/devx/usr/bin/cc
