2462b15d70c34914315482550747cb8ba2ceb640
PB <peabee@users.noreply.github.com>
2026-03-10 17:27:10 +0000

    Update devx-hack.sh

--- woof-distro/x86_64/ubuntu/resolute64/support/resolute64/gcc_permissive/devx-hack.sh
+++ woof-distro/x86_64/ubuntu/resolute64/support/resolute64/gcc_permissive/devx-hack.sh
@@ -1,4 +1,3 @@
-echo "For resolute devx: gcc permissive"
-cp -vf ./support/resolute64/gcc_permissive/gcc ./sandbox3/devx/usr/bin/
-chmod +x ./sandbox3/devx/usr/bin/gcc
-ln -svf /usr/bin/gcc ./sandbox3/devx/usr/bin/cc
+echo "For resolute with GCC-15: patch petbuilds.sh"
+sed -i 's%ccache gcc\"%ccache gcc -fpermissive -std=gnu11\"%' support/petbuilds.sh
+sed -i 's%ccache g++\"%ccache g++ -std=c++98\"%' support/petbuilds.sh
