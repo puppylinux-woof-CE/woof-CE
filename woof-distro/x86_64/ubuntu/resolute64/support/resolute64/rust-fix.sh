a4692883040e2b7b4c9e8774c58b2db87b1c8503
PB <peabee@users.noreply.github.com>
2026-03-28 09:50:47 +0000

    Add files via upload

---
+++ woof-distro/x86_64/ubuntu/resolute64/support/resolute64/rust-fix.sh
@@ -0,0 +1,12 @@
+cd usr/bin
+list=`ls *-FULL | sed 's/-FULL//g'`
+
+for ii in $list
+do
+  case $ii in
+    logger|more|mount|ps|su|umount) continue ;;
+  esac
+  mv $ii-FULL ../lib/cargo/bin/coreutils/$ii
+  echo "exec /lib/cargo/bin/coreutils/$ii" > $ii-FULL
+  chmod 755 $ii-FULL
+done
