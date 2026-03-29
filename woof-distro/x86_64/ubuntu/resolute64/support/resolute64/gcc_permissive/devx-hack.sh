3dc34b0c50120b4188435e0941c920cc2a87f08b
PB <peabee@users.noreply.github.com>
2026-03-29 13:49:50 +0100

    Update devx-hack.sh

--- woof-distro/x86_64/ubuntu/resolute64/support/resolute64/gcc_permissive/devx-hack.sh
+++ woof-distro/x86_64/ubuntu/resolute64/support/resolute64/gcc_permissive/devx-hack.sh
@@ -1,4 +1,3 @@
 echo "For resolute with GCC-15: patch petbuilds.sh"
 sed -i 's%ccache gcc\"%ccache gcc -fpermissive -std=gnu11\"%' support/petbuilds.sh
 sed -i 's%ccache g++\"%ccache g++ -std=c++98\"%' support/petbuilds.sh
-sh support/resolute64/rust-fix.sh
