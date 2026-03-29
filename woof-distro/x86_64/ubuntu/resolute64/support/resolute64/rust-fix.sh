e1150a32ed4f0f36c7e2588c7e26f84b5a7173b0
PB <peabee@users.noreply.github.com>
2026-03-29 15:20:00 +0100

    Update rust-fix.sh

--- woof-distro/x86_64/ubuntu/resolute64/support/resolute64/rust-fix.sh
+++ woof-distro/x86_64/ubuntu/resolute64/support/resolute64/rust-fix.sh
@@ -7,7 +7,7 @@ do
     logger|more|mount|ps|su|umount) continue ;;
   esac
   mv $ii-FULL ../lib/cargo/bin/coreutils/$ii
-  echo "exec /lib/cargo/bin/coreutils/$ii" > $ii-FULL
+  echo "exec /lib/cargo/bin/coreutils/$ii \"\$@\"" > $ii-FULL
   chmod 755 $ii-FULL
 done
 cd ../../
