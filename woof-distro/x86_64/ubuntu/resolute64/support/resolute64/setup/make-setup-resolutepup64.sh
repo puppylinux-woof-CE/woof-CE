ee85aec4fde0a32f332ee4fcb6e83c58a1eb0ed2
PB <peabee@users.noreply.github.com>
2026-04-19 17:44:53 +0100

    Add files via upload

--- woof-distro/x86_64/ubuntu/resolute64/support/resolute64/setup/make-setup-resolutepup64.sh
+++ woof-distro/x86_64/ubuntu/resolute64/support/resolute64/setup/make-setup-resolutepup64.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Setup for ResolutePup64
 
-# 260327
+# 260412
 # gftp & wireless-tools from noble
 # efilinux from TrixiePup64-Retro
+# gdk-pixbuf from TrixiePup64 (reverts Rust glycin version)
 
-./createsfs -r new -f rp64setup gftp-common_2.9.1~beta-2build2_all.deb gftp-gtk_2.9.1~beta-2build2_amd64.deb wireless-tools_30~pre9-16.1ubuntu2_amd64.deb efilinux.sfs tweaks.sfs
+./createsfs -r new -f rp64setup gftp-common_2.9.1~beta-2build2_all.deb gftp-gtk_2.9.1~beta-2build2_amd64.deb wireless-tools_30~pre9-16.1ubuntu2_amd64.deb efilinux.sfs gdk-pixbuf-revert.sfs weechat-shell.pet tweaks.sfs
 
 FINALSFS=rp64setup_new
 rm $FINALSFS/pet.specs
