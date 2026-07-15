92bc3daf017dd9f7b19f21dc2ba99c2fca7c1ad9
PB <peabee@users.noreply.github.com>
2026-07-15 09:51:41 +0100

    TrixiePup-Retro tweaks & improvements (#4448)
    * TP64-Retro tweaks
    
    * TP32-Retro tweaks
    
    * Update _00build.conf tp64 defaultaudioplayer=mpv
    
    * Update _00build.conf tp32-retro defaultaudioplayer=mpv
    
    * extra tweak

--- woof-distro/x86/debian/dpupt32/support/trixie32/setup/make-setup-trixiepup32.sh
+++ woof-distro/x86/debian/dpupt32/support/trixie32/setup/make-setup-trixiepup32.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Setup for TrixiePup32
 
-# 250826
+# 260713
 
-./createsfs -r new -f tp32setup abiword.pet BW_rightclick.pet xfe_setup.pet gatotray-3.3-x86.pet samba_simple_setup.pet weechat-shell.pet gftp-gtk_2.9.1~beta-3_i386.deb isomaster_1.3.13-1+b2_i386.deb parcellite_1.2.1-8+b1_i386.deb config-tweaks.sfs
+./createsfs -r new -f tp32setup abiword.pet BW_rightclick.pet xfe_setup.pet gatotray-3.3-x86.pet samba_simple_setup.pet weechat-shell.pet gftp-gtk_2.9.1~beta-3_i386.deb cdrtools-3.02a09-i686_common32.pet pupmtp-1.2.pet isomaster_1.3.13-1+b2_i386.deb parcellite_1.2.1-8+b1_i386.deb config-tweaks.sfs
 
 FINALSFS=tp32setup_new
 rm $FINALSFS/pet.specs
--- woof-distro/x86_64/debian/dpupt64retro/support/trixie64/setup/make-setup-trixiepup64.sh
+++ woof-distro/x86_64/debian/dpupt64retro/support/trixie64/setup/make-setup-trixiepup64.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Setup for TrixiePup64
 
-# 250327
+# 260713
 
-./createsfs -r new -f tp64setup abiword.pet BW_rightclick.pet xfe_setup.pet gatotray-3.3_bw64.pet samba_simple_setup.pet weechat-shell.pet gftp-gtk_2.9.1~beta-3_amd64.deb isomaster_1.3.13-1+b2_amd64.deb parcellite_1.2.1-8+b1_amd64.deb config-tweaks.sfs
+./createsfs -r new -f tp64setup abiword.pet BW_rightclick.pet xfe_setup.pet gatotray-3.3_bw64.pet samba_simple_setup.pet weechat-shell.pet gftp-gtk_2.9.1~beta-3_amd64.deb cdrtools-3.02a09_trixiepup64.pet pupmtp-1.2.1_bw64.pet isomaster_1.3.13-1+b2_amd64.deb parcellite_1.2.1-8+b1_amd64.deb config-tweaks.sfs
 
 FINALSFS=tp64setup_new
 rm $FINALSFS/pet.specs
